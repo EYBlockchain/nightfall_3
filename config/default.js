e6c8472e942a52500edad7d73bc7ec4873bee697
westlad <duncan.westland@uk.ey.com>
2020-09-29 17:22:50 +0100

    feat: setup for deposit and transfer

--- config/default.js
+++ config/default.js
@@ -2,14 +2,14 @@ module.exports = {
   // These defaults are used if not set by an environment variable
   PROTOCOL: 'http://', // connect to zokrates microservice like this
   CIRCUITS_HOME: process.env.CIRCUITS_HOME || '/app/circuits/',
-  INNER_CHECKS_VK_TEMPLATE_PATH:
-    process.env.INNER_CHECKS_VK_TEMPLATE_PATH || 'zvm/2in2out/inner-checks-vk.zokm',
-  INNER_CHECKS_VK_PATH: process.env.INNER_CHECKS_VK_PATH || 'zvm/2in2out/inner-checks-vk.zok',
-  INNER_CHECKS_PATH: process.env.INNER_CHECKS_PATH || 'zvm/2in2out/inner-checks.zok',
-  OUTER_VERIFICATION_PATH:
-    process.env.OUTER_VERIFICATION_PATH || 'zvm/2in2out/outer-verification.zok',
-  ALWAYS_DO_TRUSTED_SETUP: process.env.ALWAYS_DO_TRUSTED_SETUP == 1, // eslint-disable-line eqeqeq, intentionally loose equality reads 1/0 as truthy/falsey
-  ZOKRATES_HOST: process.env.ZOKRATES_HOST || 'zokrates',
+  DEPOSIT_PATH: process.env.DEPOSIT_PATH || 'deposit.zok',
+  TRANSFER_PATH: process.env.TRANSFER_PATH || 'transfer.zok',
+  WITHDRAW_PATH: process.env.WITHDRAW_PATH || 'withdraw.zok',
+  get TRUSTED_SETUPS_TODO() {
+    return [this.DEPOSIT_PATH, this.TRANSFER_PATH];
+  },
+  ALWAYS_DO_TRUSTED_SETUP: process.env.ALWAYS_DO_TRUSTED_SETUP || false,
+  ZOKRATES_WORKER_HOST: process.env.ZOKRATES_WORKER_HOST || 'worker',
   BLOCKCHAIN_HOST: process.env.BLOCKCHAIN_HOST || 'ws://openethereum',
   BLOCKCHAIN_PORT: process.env.BLOCKCHAIN_PORT || '8546',
   WEB3_OPTIONS: {
--- src/circuit-setup.mjs
+++ src/circuit-setup.mjs
@@ -4,13 +4,11 @@ instance. Note, we don't need to deploy the circuits through a zokrates microser
 */
 import axios from 'axios';
 import config from 'config';
-import { writeVk } from './write-vk.mjs';
-import { registerOuterVK, getOuterVK } from './zvm.mjs';
 import logger from './utils/logger.mjs';
-import Web3 from './utils/web3.mjs';
-import { generalise } from './utils/general-number.mjs';
+// import Web3 from './utils/web3.mjs';
+// import { generalise } from './utils/general-number.mjs';
 
-const web3 = Web3.connection();
+// const web3 = Web3.connection();
 
 /**
 This function will ping the Zokrates service until it is up before attempting
@@ -18,79 +16,55 @@ to use it. This is because the deployer must start before Zokrates as it needs
 to populate Zokrates' volumes.  Thus it can't be sure that Zokrates is up yet
 */
 async function waitForZokrates() {
-  logger.info('checking for zokrates_zexe_microservice');
+  logger.info('checking for zokrates_worker');
   while (
-    (await axios.get(`${config.PROTOCOL}${config.ZOKRATES_HOST}/healthcheck`)).status !== 200
+    (await axios.get(`${config.PROTOCOL}${config.ZOKRATES_WORKER_HOST}/healthcheck`)).status !== 200
   ) {
     logger.warn(
-      `No response from zokrates_zexe_microservice yet.  That's ok. We'll wait three seconds and try again...`,
+      `No response from zokratesworker yet.  That's ok. We'll wait three seconds and try again...`,
     );
     await new Promise(resolve => setTimeout(resolve, 3000));
   }
-  logger.info('zokrates_zexe_microservice reports that it is healthy');
+  logger.info('zokrates_worker reports that it is healthy');
 }
 
 /**
 This calls the /generateKeys endpoint on a zokrates microservice container to do the setup.
 */
 async function setupCircuits() {
-  // First check if we already have an outer vk registered
-  let vk;
-  try {
-    if (!config.OUTER_VERIFICATION_PATH.endsWith('.zok'))
-      throw new Error('Outer verification path must end with .zok');
-    const folderpath = config.OUTER_VERIFICATION_PATH.slice(0, -4);
-    logger.debug('Checking for existing vk');
-    const response = await axios.get(`${config.PROTOCOL}${config.ZOKRATES_HOST}/vk`, {
-      data: {
-        folderpath,
-      },
+  // do all the trusted setups needed
+  for (const path of config.TRUSTED_SETUPS_TODO) {
+    // first check if a vk already exists
+    let vk;
+    logger.debug(`checking for existing setup for ${path}`);
+    const folderpath = path.slice(0, -4); // remove the .zok extension
+    const res1 = await axios.get(`${config.PROTOCOL}${config.ZOKRATES_WORKER_HOST}/vk`, {
+      params: { folderpath },
     });
-    ({ vk } = response.data);
-    logger.debug(`GET vk response was ${JSON.stringify(vk, null, 2)}`);
-  } catch (err) {
-    logger.error(err);
+    vk = res1.data.vk;
+    if (!vk || config.ALWAYS_DO_TRUSTED_SETUP) {
+      // we don't have an existing vk so let's generate one
+      try {
+        logger.debug(`no existing verification key: calling generate keys on ${path}`);
+        const res2 = await axios.post(
+          `${config.PROTOCOL}${config.ZOKRATES_WORKER_HOST}/generate-keys`,
+          {
+            filepath: path,
+            curve: 'bn128',
+            provingScheme: 'gm17',
+            backend: 'libsnark',
+          },
+        );
+        vk = res2.data.vk;
+      } catch (err) {
+        logger.error(err);
+      }
+    } else logger.info(`${path} verification key exists: trusted setup skipped`);
+    // we should register the vk now
+    logger.warn('VK REGISTERING NOT YET IMPLEMENTED');
   }
-  // if not...
-  // Generate the keys for the inner-checks circuit.  We need to capture the vk
-  // and insert it into the outer verification circuit
-  if (!vk || config.ALWAYS_DO_TRUSTED_SETUP) {
-    logger.info('Starting trusted setup');
-    try {
-      logger.debug('calling generate keys on inner checks circuit');
-      const response = await axios.post(`${config.PROTOCOL}${config.ZOKRATES_HOST}/generate-keys`, {
-        filepath: config.INNER_CHECKS_PATH,
-        curve: 'bls12_377',
-        provingScheme: 'gm17',
-        backend: 'zexe',
-      });
-      vk = response.data.vk;
-      delete vk.raw; // long and not needed
-      logger.silly('generate-keys responded with vk of:', vk);
-    } catch (err) {
-      logger.error(err);
-    }
-    // now create a .zok file containing the vk
-    try {
-      writeVk(vk);
-    } catch (err) {
-      logger.error(err);
-    }
-    // and, having done that, we can set up the outer verifier
-    try {
-      logger.debug('calling generate keys on outer verification circuit');
-      const response = await axios.post(`${config.PROTOCOL}${config.ZOKRATES_HOST}/generate-keys`, {
-        filepath: config.OUTER_VERIFICATION_PATH,
-        curve: 'bw6_761',
-        provingScheme: 'gm17',
-        backend: 'zexe',
-      });
-      vk = response.data.vk;
-    } catch (err) {
-      logger.error(err);
-    }
-  } else logger.info('Trusted setup skipped');
   // and, lastly, we register the outer vk with ZVM.sol
+  /*
   try {
     logger.debug('Registering outer verification vk');
     delete vk.raw; // long and not needed
@@ -117,7 +91,8 @@ async function setupCircuits() {
     logger.error(err);
     throw new Error(err);
   }
-  Web3.disconnect();
+  */
+  // Web3.disconnect();
 }
 
 export default { setupCircuits, waitForZokrates };
