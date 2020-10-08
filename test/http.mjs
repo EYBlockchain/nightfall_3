1bf6d9a762048afe7ea71f6eedd25027cd9be1d5
westlad <duncan.westland@uk.ey.com>
2020-10-08 09:56:38 +0100

    fix: update tests to show API

--- test/http.mjs
+++ test/http.mjs
@@ -7,10 +7,14 @@ chai.use(chaiHttp);
 
 describe('Testing the http API', () => {
   let id;
+  // the address is for the OpsCoin contract that's on main-net: just as a dummy value.
+  const ercAddress = '0x409D23C77aAC746Ef5d57B5b44e628Ee4daa0F26';
+  const url = 'http://localhost:8080';
+  const tokenId = '0x01';
 
   it('should respond with status 200 to the health check', done => {
     chai
-      .request('http://localhost:8080')
+      .request(url)
       .get('/healthcheck')
       .end((err, res) => {
         expect(res.status).to.equal(200);
@@ -20,7 +24,7 @@ describe('Testing the http API', () => {
 
   it('should generate a 256 bit zkp private key for a user', done => {
     chai
-      .request('http://localhost:8080')
+      .request(url)
       .get('/generate-zkp-key')
       .end((err, res) => {
         expect(res.body.keyId).to.be.a('string');
@@ -29,29 +33,54 @@ describe('Testing the http API', () => {
       });
   });
 
-  it('should deposit some crypto into a ZKP commitment', async () => {
-    // the address is for the OpsCoin that's on main-net, just as a dummy value
-    const res = await request.post('/deposit').send({
-      ercAddress: '0x409D23C77aAC746Ef5d57B5b44e628Ee4daa0F26',
-      tokenId: 0x01,
-      value: 10,
-      zkpPublicKey: id,
-    });
-    console.log(res.body);
-    // expect(res.body.txToSign).to.be.a('string');
+  it('should deposit some crypto into a ZKP commitment', done => {
+    chai
+      .request(url)
+      .post('/deposit')
+      .send({
+        ercAddress,
+        tokenId: 0x01,
+        value: 10,
+        zkpPublicKey: id,
+      })
+      .end((err, res) => {
+        expect(res.body.txToSign).to.be.a('string');
+        console.log(res.body);
+        done();
+      });
   });
 
   it.skip('should transfer some crypto using ZKP', async () => {
-    const res = await request
+    chai
+      .request(url)
       .post('/transfer')
-      .send({ type: types.FUNGIBLE, value: 10, id: 1234, recipientId: 3456 });
-    expect(res.body.txToSign).to.be.a('string');
+      .send({
+        ercAddress,
+        tokenId,
+        value,
+        senderZkpPublicKey,
+        recipientZkpPublicKey
+      })
+      .end((err, res) => {
+        // TODO
+        expect(res.body.txToSign).to.be.a('string');
+      });
   });
 
   it.skip('should withdraw some crypto from a ZKP commitment', async () => {
-    const res = await request
+    chai
+      .request(url)
       .post('/withdraw')
-      .send({ type: types.FUNGIBLE, value: 10, id: 1234, recipientAddress: '0x1234' });
-    expect(res.body.txToSign).to.be.a('string');
+      .send({
+        ercAddress,
+        tokenId,
+        value,
+        senderZkpPublicKey,
+        recipientAddress
+      })
+      .end((err, res) => {
+        // TODO
+        expect(res.body.txToSign).to.be.a('string');
+      });
   });
 });
