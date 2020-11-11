420fda7327fabc7fe49ba97b01d4ec6b204a9ecf
westlad <duncan.westland@uk.ey.com>
2020-11-11 14:35:01 +0000

    fix: autostart waits for shield contract deployment

--- merkle-tree/src/auto-start.js
+++ merkle-tree/src/auto-start.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import config from 'config';
 import logger from './logger';
+import uw from './utils-web3';
 /**
 function to automatically start the Timber instance.  This is useful if you are
 starting it up in an already-established blockchain environment.
@@ -14,6 +15,27 @@ const autoStart = async () => {
       contractAddress: config.contracts[contractName].address,
       treeId: config.contracts[contractName].treeId,
     };
+    // if we haven't been given a contractAddress, it's best to make sure that
+    // Timber has the ability to infer one. Otherwise Timber will fall over. If
+    // the build artefacts don't exist yet then we need to wait.
+    // Stop eslint complaining because we DO want delays in this loop
+    let retries = 20;
+    while (!data.contractAddress) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        data.contractAddress = await uw.getContractAddress(contractName);
+        // we want to catch if the above function throws OR returns undefined
+        if (data.contractAddress === undefined) throw new Error('undefined contract address');
+        logger.info(`Contract address for contract ${contractName} is ${data.contractAddress}`);
+      } catch (err) {
+        if (retries === 0) throw new Error(err);
+        retries--;
+        logger.warn('Unable to find a contract address. Retrying in 3 seconds.');
+        // eslint-disable-next-line no-await-in-loop
+        await new Promise(resolve => setTimeout(resolve, 3000));
+      }
+    }
+    // Now that we are fairly sure calling start will work, we can go ahead.
     try {
       logger.debug(
         `Calling /start for Timber, with contractName '${contractName}' and url localhost`,
