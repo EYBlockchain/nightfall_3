9954adf956a5180ddc82b17674d322f07fea622f
Liju Jose <liju.jose@gds.ey.com>
2021-05-06 17:07:52 +0530

    fix: eslint prettier and husky configured

---
+++ consolidated-eslint-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['codfish'],
+  rules: {
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'import/extensions': 'off',
+    'no-restricted-syntax': 'off',
+    'no-plusplus': 'off',
+  },
+  globals: {
+    BigInt: 'true',
+  },
+  env: {
+    mocha: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+};
