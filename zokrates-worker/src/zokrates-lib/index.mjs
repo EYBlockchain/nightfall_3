9580df1f085ced9f1498f61ff191175ca8d10a61
Duncan Westland <duncan.westland@uk.ey.com>
2021-06-08 12:41:22 +0100

    Revert "npm module zokrates-zexe.js removed"

--- zokrates-worker/src/zokrates-lib/index.mjs
+++
@@ -1,8 +0,0 @@
-import compile from './compile.mjs';
-import computeWitness from './compute-witness.mjs';
-import extractVk from './extract-vk.mjs';
-import generateProof from './generate-proof.mjs';
-import setup from './setup.mjs';
-import verify from './verify.mjs';
-
-export { compile, computeWitness, extractVk, generateProof, setup, verify };
