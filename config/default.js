6314702a631f2d03d5e3c4595178219c467d3d8a
Liju Jose <liju.jose@gds.ey.com>
2021-06-17 17:56:43 +0530

    fix: conflict resolved

--- config/default.js
+++ config/default.js
@@ -43,24 +43,15 @@ module.exports = {
   TRANSACTIONS_PER_BLOCK: Number(process.env.TRANSACTIONS_PER_BLOCK) || 2,
   TIMBER_SYNC_RETRIES: 5, // Sets amount of exponential backoff attempts to sync with timber.
   PROPOSE_BLOCK_TYPES: [
-    '(address,bytes32,uint64,uint64, uint64)',
-    '(uint64,uint64, uint8,bytes32,bytes32,bytes32,bytes32,bytes32[2],bytes32[2],uint[4])[]',
-  ], // used to encode/decode proposeBlock signature
-  SUBMIT_TRANSACTION_TYPES:
-    '(uint64,uint64, uint8,bytes32,bytes32,bytes32,bytes32,bytes32[2],bytes32[2],uint[4])',
-  RETRIES: 50,
-  NODE_HASHLENGTH: 32,
-  ZERO: '0x0000000000000000000000000000000000000000000000000000000000000000',
-  HASH_TYPE: 'mimc',
-  TRANSACTIONS_PER_BLOCK: Number(process.env.TRANSACTIONS_PER_BLOCK) || 2,
-  TIMBER_SYNC_RETRIES: 5, // Sets amount of exponential backoff attempts to sync with timber.
-  PROPOSE_BLOCK_TYPES: [
     '(uint48,uint48,address,bytes32)',
     '(uint64,uint64,uint8,bytes32,bytes32,bytes32,bytes32,bytes32[2],bytes32[2],uint[4])[]',
   ], // used to encode/decode proposeBlock signature
   SUBMIT_TRANSACTION_TYPES:
     '(uint64,uint64,uint8,bytes32,bytes32,bytes32,bytes32,bytes32[2],bytes32[2],uint[4])',
+  RETRIES: 50,
+  NODE_HASHLENGTH: 32,
+  ZERO: '0x0000000000000000000000000000000000000000000000000000000000000000',
+  HASH_TYPE: 'mimc',
   USE_STUBS: process.env.USE_STUBS === 'true',
-  PEERS_COLLECTION: 'peers',
   VK_IDS: { deposit: 0, single_transfer: 1, double_transfer: 2, withdraw: 3 }, // used as an enum to mirror the Shield contracts enum for vk types. The keys of this object must correspond to a 'folderpath' (the .zok file without the '.zok' bit)
 };
