0a8efdd2726eeaf2f47bc55e67dc55d88c8fba03
Hari <harikrishnan.m@gds.ey.com>
2021-09-17 15:03:20 +0530

    change currentProposer to next aaddress irrespective of whether proposer is currentProposer - remove commented lines

--- nightfall-deployer/contracts/State.sol
+++ nightfall-deployer/contracts/State.sol
@@ -154,13 +154,11 @@ contract State is Structures, Config {
     delete proposers[proposer];
     proposers[previousAddress].nextAddress = proposers[nextAddress].thisAddress;
     proposers[nextAddress].previousAddress = proposers[previousAddress].thisAddress;
-   // if(proposer == currentProposer.thisAddress) {
       // Cannot just call changeCurrentProposer directly due to the require time check
       //change currentProposer to next aaddress irrespective of whether proposer is currentProposer
       proposerStartBlock = block.number;
       currentProposer = proposers[nextAddress];
       emit NewCurrentProposer(currentProposer.thisAddress);
-   // }
   }
   // Checks if a block is actually referenced in the queue of blocks waiting
   // to go into the Shield state (stops someone challenging with a non-existent
