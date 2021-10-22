df6d42775e8a55046f74cff5181987aeae3aad18
ilyasridhuan <ilyasridhuan@gmail.com>
2021-10-22 14:20:30 +0800

    fix: update drop script

--- nightfall-client/dropAll.sh
+++ nightfall-client/dropAll.sh
@@ -12,5 +12,6 @@ docker exec -t nightfall_3_optimist1_1 bash -c \
     db.blocks.drop( { writeConcern: { w: \"majority\" } } );
     db.nullifiers.drop( { writeConcern: { w: \"majority\" } } );
     db.transactions.drop( { writeConcern: { w: \"majority\" } } );
+    db.timber.drop( { writeConcern: { w: \"majority\" } } );
     db.metadata.drop( { writeConcern: { w: \"majority\" } } );
     db.metadata.insert( { \"_id\" : ObjectId(\"60913a97d2515d00e807ba07\"), \"proposer\" : \"0x9C8B2276D490141Ae1440Da660E470E7C0349C63\" })"'
