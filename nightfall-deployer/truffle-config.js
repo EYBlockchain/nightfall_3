d1afbd2338bdda1b69f21c6540a4fcaa3ad30b7a
Liju Jose <liju.jose@gds.ey.com>
2021-10-22 22:10:23 +0530

    test: 2.19

--- nightfall-deployer/truffle-config.js
+++ nightfall-deployer/truffle-config.js
@@ -87,8 +87,9 @@ module.exports = {
       provider: () => new HDWalletProvider(config.ETH_PRIVATE_KEY, config.BLOCKCHAIN_URL),
       network_id: 3,
       gas: 7992189,
+      gasPrice: 100000000000000000
       networkCheckTimeout: 100000000,
-      timeoutBlocks: 1000,
+      timeoutBlocks: 20000,
       skipDryRun: true,
       disableConfirmationListener: true,
     },
