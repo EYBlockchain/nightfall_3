3c1c712f50f6cdeea8897a0f92b7728a987e4853
daveroga <daveroga@gmail.com>
2021-10-26 17:07:59 +0200

    fix: add deployment of ERC20, ERC721, ERC1155 mock contracts #245

--- test/e2e/ercmocks.test.mjs
+++ test/e2e/ercmocks.test.mjs
@@ -21,44 +21,44 @@ describe('Testing the Nightfall SDK', () => {
   );
 
   // The minimum ABI to get ERC20 Token balance, decimals and transfer functions
-  let minABI = [
+  const minABI = [
     // balanceOf
     {
-      "constant":true,
-      "inputs":[{"name":"_owner","type":"address"}],
-      "name":"balanceOf",
-      "outputs":[{"name":"balance","type":"uint256"}],
-      "type":"function"
+      constant: true,
+      inputs: [{ name: '_owner', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ name: 'balance', type: 'uint256' }],
+      type: 'function',
     },
     // decimals
     {
-      "constant":true,
-      "inputs":[],
-      "name":"decimals",
-      "outputs":[{"name":"","type":"uint8"}],
-      "type":"function"
+      constant: true,
+      inputs: [],
+      name: 'decimals',
+      outputs: [{ name: '', type: 'uint8' }],
+      type: 'function',
     },
     // transfer
     {
-      "constant": false,
-      "inputs": [
+      constant: false,
+      inputs: [
         {
-          "name": "_to",
-          "type": "address"
+          name: '_to',
+          type: 'address',
         },
         {
-          "name": "_value",
-          "type": "uint256"
-        }
+          name: '_value',
+          type: 'uint256',
+        },
       ],
-      "name": "transfer",
-      "outputs": [
+      name: 'transfer',
+      outputs: [
         {
-          "name": "",
-          "type": "bool"
-        }
+          name: '',
+          type: 'bool',
+        },
       ],
-      "type": "function"
+      type: 'function',
     },
   ];
 
