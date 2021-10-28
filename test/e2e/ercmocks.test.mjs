3cbeaf07f23e5f3433d084abf4f9b8c75df5ab42
daveroga <daveroga@gmail.com>
2021-10-28 22:46:52 +0200

    fix: transfer tokens to wallet test address

--- test/e2e/ercmocks.test.mjs
+++ test/e2e/ercmocks.test.mjs
@@ -12,6 +12,7 @@ chai.use(chaiAsPromised);
 
 describe('Testing the Nightfall SDK', () => {
   const ethereumSigningKey = '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e';
+  const walletTestAddress = '0xDd369AF1Bd4D8fEBEf2cE1716AfEC592e00553AA'; 
   const nf3 = new Nf3(
     'http://localhost:8080',
     'http://localhost:8081',
@@ -20,9 +21,9 @@ describe('Testing the Nightfall SDK', () => {
     ethereumSigningKey,
   );
 
-  // The minimum ABI to get ERC20 Token balance, decimals and transfer functions
+  // The minimum ABI to get ERC20, ERC721 and ERC1155 basic functions
   const minABI = [
-    // balanceOf
+    // balanceOf ERC20, ERC721
     {
       constant: true,
       inputs: [{ name: '_owner', type: 'address' }],
@@ -30,7 +31,18 @@ describe('Testing the Nightfall SDK', () => {
       outputs: [{ name: 'balance', type: 'uint256' }],
       type: 'function',
     },
-    // decimals
+    // balanceOf ERC1155
+    {
+      constant: true,
+      inputs: [
+        { name: '_owner', type: 'address' },
+        { name: '_tokenId', type: 'uint256' },
+      ],
+      name: 'balanceOf',
+      outputs: [{ name: 'balance', type: 'uint256' }],
+      type: 'function',
+    },
+    // decimals ERC20
     {
       constant: true,
       inputs: [],
@@ -38,7 +50,7 @@ describe('Testing the Nightfall SDK', () => {
       outputs: [{ name: '', type: 'uint8' }],
       type: 'function',
     },
-    // transfer
+    // transfer ERC20
     {
       constant: false,
       inputs: [
@@ -58,6 +70,56 @@ describe('Testing the Nightfall SDK', () => {
           type: 'bool',
         },
       ],
+      type: 'function',
+    },
+    // safeTransferFrom ERC721
+    {
+      constant: false,
+      inputs: [
+        {
+          name: '_from',
+          type: 'address',
+        },
+        {
+          name: '_to',
+          type: 'address',
+        },
+        {
+          name: '_tokenId',
+          type: 'uint256',
+        },
+      ],
+      name: 'safeTransferFrom',
+      outputs: [],
+      type: 'function',
+    },
+    // safeTransferFrom ERC1155
+    {
+      constant: false,
+      inputs: [
+        {
+          name: '_from',
+          type: 'address',
+        },
+        {
+          name: '_to',
+          type: 'address',
+        },
+        {
+          name: '_id',
+          type: 'uint256',
+        },
+        {
+          name: '_amount',
+          type: 'uint256',
+        },
+        {
+          name: '_data',
+          type: 'bytes',
+        },
+      ],
+      name: 'safeTransferFrom',
+      outputs: [],
       type: 'function',
     },
   ];
@@ -119,6 +181,12 @@ describe('Testing the Nightfall SDK', () => {
       expect(erc20Address).to.be.a('string').and.to.include('0x');
     });
 
+    it('should get the balance of wallet address of the ERC20 contract mock', async function () {
+      const erc20Token = new web3.eth.Contract(minABI, erc20Address);
+      const balance = await erc20Token.methods.balanceOf(walletTestAddress).call();
+      expect(Number(balance)).to.be.greaterThan(0);
+    });
+
     it('should get the balance of the ERC20 contract mock', async function () {
       const erc20Token = new web3.eth.Contract(minABI, erc20Address);
       const amount = 3000;
@@ -139,15 +207,39 @@ describe('Testing the Nightfall SDK', () => {
       expect(erc721Address).to.be.a('string').and.to.include('0x');
     });
 
+    it('should get the balance of the ERC721 contract mock', async function () {
+      const erc721Token = new web3.eth.Contract(minABI, erc721Address);
+      const balanceBefore = await erc721Token.methods.balanceOf(nf3.ethereumAddress).call();
+      expect(Number(balanceBefore)).to.be.greaterThan(0);
+    });
+
+    it('should transfer one token of the ERC721 contract mock', async function () {
+      const erc721Token = new web3.eth.Contract(minABI, erc721Address);
+      const balanceBefore = await erc721Token.methods.balanceOf(nf3.ethereumAddress).call();
+      const res = await erc721Token.methods
+        .safeTransferFrom(nf3.ethereumAddress, walletTestAddress, 3)
+        .send({ from: nf3.ethereumAddress });
+      expect(res).to.have.property('transactionHash');
+      expect(res).to.have.property('blockHash');
+      const balanceAfter = await erc721Token.methods.balanceOf(nf3.ethereumAddress).call();
+      expect(Number(balanceAfter)).to.be.equal(Number(balanceBefore) - 1);
+    });
+
     it('should get the address of the test ERC1155 contract mock', async function () {
       erc1155Address = await nf3.getContractAddress('ERC1155Mock');
       expect(erc1155Address).to.be.a('string').and.to.include('0x');
     });
 
-    it('should subscribe to block proposed event with the provided incoming viewing key for client', async function () {
-      const res = await nf3.subscribeToIncomingViewingKeys();
-      expect(res.data.status).to.be.a('string');
-      expect(res.data.status).to.equal('success');
+    it('should get the balance of the ERC1155 contract mock', async function () {
+      const erc1155Token = new web3.eth.Contract(minABI, erc1155Address);
+      const balance = await erc1155Token.methods.balanceOf(nf3.ethereumAddress, 1).call();
+      expect(Number(balance)).to.be.greaterThan(0);
+    });
+
+    it('should get the balance of wallet address of the ERC1155 contract mock', async function () {
+      const erc1155Token = new web3.eth.Contract(minABI, erc1155Address);
+      const balance = await erc1155Token.methods.balanceOf(walletTestAddress, 1).call();
+      expect(Number(balance)).to.be.greaterThan(0);
     });
   });
 
