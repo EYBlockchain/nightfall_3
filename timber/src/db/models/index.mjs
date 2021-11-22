dfa05a19bada81b648d4a972ec7d2ca6337813a4
ilyasridhuan <ilyasridhuan@gmail.com>
2021-11-22 09:20:43 +0800

    refactor: remove timber folder

--- timber/src/db/models/index.mjs
+++
@@ -1,3 +0,0 @@
-export { default as nodeSchema } from './node.model.mjs';
-export { default as metadataSchema } from './metadata.model.mjs';
-export { default as historySchema } from './history.model.mjs';
