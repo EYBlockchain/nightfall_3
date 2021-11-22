7576f4e192fdc514cfcc7b6bb5fae4acb8e7778e
Westlad <Duncan.Westland@uk.ey.com>
2021-11-22 09:43:27 +0000

    feat: basic dir structure

---
+++ test/ping-pong/proposer/src/config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  optimistWsUrl: 'ws://optimist:8080',
+  web3WsUrl: 'ws://blockchain1:8546',
+  ethereumSigningKey: '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69d',
+  defaultMnemonic: 'hurt labor ketchup seven scan swap dirt brown brush path goat together',
+};
+
+export default config;
