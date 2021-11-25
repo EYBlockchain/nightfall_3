9b5606d6f42a414aaaee5cb9a9a1cd08b54d4e52
Westlad <Duncan.Westland@uk.ey.com>
2021-11-25 17:11:15 +0000

    fix: config

--- test/ping-pong/proposer/src/index.mjs
+++ test/ping-pong/proposer/src/index.mjs
@@ -2,6 +2,7 @@
 Module that runs up as a proposer
 */
 import config from 'config';
+import logger from 'common-files/utils/logger.mjs';
 import Nf3 from './nf3.mjs';
 
 const { proposerEthereumSigningKey, optimistWsUrl, web3WsUrl, clientBaseUrl, optimistBaseUrl } =
@@ -11,7 +12,7 @@ const { proposerEthereumSigningKey, optimistWsUrl, web3WsUrl, clientBaseUrl, opt
 Does the preliminary setup and starts listening on the websocket
 */
 async function startProposer() {
-  console.log('Starting Proposer...');
+  logger.info('Starting Proposer...');
   const nf3 = new Nf3(
     clientBaseUrl,
     optimistBaseUrl,
@@ -21,13 +22,13 @@ async function startProposer() {
   );
   await nf3.init();
   if ((await nf3.healthcheck('optimist')) && (await nf3.healthcheck('client')))
-    console.log('Healthcheck passed');
+    logger.info('Healthcheck passed');
   else throw new Error('Healthcheck failed');
   await nf3.registerProposer();
-  console.log('Proposer registration complete');
+  logger.debug('Proposer registration complete');
   // TODO subscribe to layer 1 blocks and call change proposer
   nf3.startProposer();
-  console.log('Listening for incoming events');
+  logger.info('Listening for incoming events');
 }
 
 startProposer();
