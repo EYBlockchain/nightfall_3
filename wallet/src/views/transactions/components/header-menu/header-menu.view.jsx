d8fc7641a3264700888215b24aaf180bbf70a0d4
Liju Jose <liju.jose@gds.ey.com>
2021-11-25 10:58:11 +0530

    fix: wasm loader added to react app

--- wallet/src/views/transactions/components/header-menu/header-menu.view.jsx
+++ wallet/src/views/transactions/components/header-menu/header-menu.view.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Menu, Icon, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { deleteWallet } from '../../../../store/login/login.actions';
 import { clearMsg } from '../../../../store/message/message.actions';
 import AccountSettingsModal from './account-settings.view.jsx';
@@ -29,6 +30,10 @@ function HeaderMenu({ onDeleteWallet, onClearMsg }) {
         <Button name="account-settings" primary onClick={() => toggleAccountSettings()}>
           <Icon name="settings" size="large" />
           Account Settings
+        </Button>
+        <Button name="zokrates">
+          <Icon name="settings" size="large" />
+          <Link to="/zokrates">zokrates</Link>
         </Button>
         <Button name="account-info" primary disabled onClick={() => handleClickNFInfo()}>
           <Icon name="question" size="large" />
