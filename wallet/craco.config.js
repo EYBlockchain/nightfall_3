895134ab5c712e0d6f42cc1d6a0e6a03aebe4c79
Liju Jose <liju.jose@gds.ey.com>
2021-11-25 10:58:11 +0530

    fix: wasm loader added to react app

--- wallet/craco.config.js
+++ wallet/craco.config.js
@@ -1,4 +1,30 @@
 /* eslint-disable global-require */
+const { addBeforeLoader, loaderByName } = require('@craco/craco');
+
 module.exports = {
   plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
+  webpack: {
+    configure: (webpackConfig, { paths }) => {
+      const wasmExtensionRegExp = /\.wasm$/;
+
+      webpackConfig.resolve.extensions.push('.wasm');
+
+      webpackConfig.module.rules.forEach((rule) => {
+        (rule.oneOf || []).forEach((oneOf) => {
+          if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+            oneOf.exclude.push(wasmExtensionRegExp);
+          }
+        });
+      });
+
+      const wasmLoader = {
+        test: wasmExtensionRegExp,
+        include: paths.appSrc,
+        loader: require.resolve('wasm-loader'),
+        options: {},
+      };
+      addBeforeLoader(webpackConfig, loaderByName('file-loader'), wasmLoader);
+      return webpackConfig;
+    },
+  },
 };
