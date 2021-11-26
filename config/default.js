e64a62673028b1588a630e4caa1d1dd8686aa6a9
Westlad <Duncan.Westland@uk.ey.com>
2021-11-26 14:06:20 +0000

    fix: externalise test keys

--- config/default.js
+++ config/default.js
@@ -37,13 +37,13 @@ module.exports = {
   optimistWsUrl: `ws://${process.env.OPTIMIST_HOST}:${process.env.OPTIMIST_WS_PORT}`,
   web3WsUrl: `ws://${process.env.BLOCKCHAIN_WS_HOST}:${process.env.BLOCKCHAIN_PORT}`,
   userEthereumSigningKey:
-    process.env.userEthereumSigningKey ||
+    process.env.USER_ETHEREUM_SIGNING_KEY ||
     '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e',
   zkpMnemonic:
-    process.env.zkpMnemonic ||
+    process.env.ZKP_MNEMONIC ||
     'hurt labor ketchup seven scan swap dirt brown brush path goat together',
   proposerEthereumSigningKey:
-    process.env.proposerEthereumSigningKey ||
+    process.env.PROPOSER_ETHEREUM_SIGNING_KEY ||
     '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69d',
   WEB3_OPTIONS: {
     gas: process.env.GAS || 1000000,
