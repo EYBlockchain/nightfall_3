8967957cabc60e8d675d3ac598928b9e8de88fbb
Westlad <Duncan.Westland@uk.ey.com>
2021-11-26 14:04:04 +0000

    fix: externalise test keys

--- config/default.js
+++ config/default.js
@@ -36,9 +36,15 @@ module.exports = {
   optimistBaseUrl: `http://${process.env.OPTIMIST_HOST}:${process.env.OPTIMIST_HTTP_PORT}`,
   optimistWsUrl: `ws://${process.env.OPTIMIST_HOST}:${process.env.OPTIMIST_WS_PORT}`,
   web3WsUrl: `ws://${process.env.BLOCKCHAIN_WS_HOST}:${process.env.BLOCKCHAIN_PORT}`,
-  userEthereumSigningKey: '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e',
-  zkpMnemonic: 'hurt labor ketchup seven scan swap dirt brown brush path goat together',
-  proposerEthereumSigningKey: '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69d',
+  userEthereumSigningKey:
+    process.env.userEthereumSigningKey ||
+    '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e',
+  zkpMnemonic:
+    process.env.zkpMnemonic ||
+    'hurt labor ketchup seven scan swap dirt brown brush path goat together',
+  proposerEthereumSigningKey:
+    process.env.proposerEthereumSigningKey ||
+    '0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69d',
   WEB3_OPTIONS: {
     gas: process.env.GAS || 1000000,
     gasPrice: process.env.GAS_PRICE || '20000000000',
