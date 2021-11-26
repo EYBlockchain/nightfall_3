4b7cf42af9a050644f1da034c18dbe9345b59ead
dwebchapey <harikrishnan.m@gds.ey.com>
2021-11-26 08:07:45 +0530

    feat: nf3 circuits in zkjs browser

--- wallet/src/views/zokrates/circuits/index.js
+++ wallet/src/views/zokrates/circuits/index.js
@@ -3,27 +3,109 @@
 // ignore unused exports
 import deposit from '!!raw-loader!./deposit.zok';
 import depositStub from '!!raw-loader!./deposit_stub.zok';
+import depositNoHash from '!!raw-loader!./deposit-nohash.zok';
+
 import doubleTransfer from '!!raw-loader!./double_transfer.zok';
 import doubleTransferStub from '!!raw-loader!./double_transfer_stub.zok';
+import doubleTransferNoHash from '!!raw-loader!./double_transfer-nohash.zok';
+
+
 import singleTransfer from '!!raw-loader!./single_transfer.zok';
 import singleTransferStub from '!!raw-loader!./single_transfer_stub.zok';
+import singleTransferNoHash from '!!raw-loader!./single_transfer-nohash.zok';
+
+
 import withdraw from '!!raw-loader!./withdraw.zok';
 import withdrawStub from '!!raw-loader!./withdraw_stub.zok';
+import withdrawNoHash from '!!raw-loader!./withdraw-nohash.zok';
 
-import pad1280ThenHash from '!!raw-loader!./common/hashes/sha256/pad1280ThenHash.zok';
-import pad1024ThenHash from '!!raw-loader!./common/hashes/sha256/pad1024ThenHash.zok';
+
+//concatenate
+import orderLeftRight from '!!raw-loader!./common/concatenate/order-left-right-1x1.zok';
+
+//encryption
+import elgamal from '!!raw-loader!./common/encryption/el-gamal4.zok';
+
+//hashes
+import mimcEncryption from '!!raw-loader!./common/hashes/mimc/mimc-encryption.zok';
+import mimcConstants from '!!raw-loader!./common/hashes/mimc/mimc-constants.zok';
+import mimcHash1 from '!!raw-loader!./common/hashes/mimc/mimc-hash-1.zok';
+import mimcHash2 from '!!raw-loader!./common/hashes/mimc/mimc-hash-2.zok';
+import mimcHash5 from '!!raw-loader!./common/hashes/mimc/mimc-hash-5.zok';
+import hash512 from '!!raw-loader!./common/hashes/sha256/hash512.zok';
+import hash1024 from '!!raw-loader!./common/hashes/sha256/hash1024.zok';
 import hash1536 from '!!raw-loader!./common/hashes/sha256/hash1536.zok';
+import hash2048 from '!!raw-loader!./common/hashes/sha256/hash2048.zok';
+import hash3584 from '!!raw-loader!./common/hashes/sha256/hash3584.zok';
+import hash4096 from '!!raw-loader!./common/hashes/sha256/hash4096.zok';
+import hash4608 from '!!raw-loader!./common/hashes/sha256/hash4608.zok';
+import pad256ThenHash from '!!raw-loader!./common/hashes/sha256/pad256ThenHash.zok';
+import pad512ThenHash from '!!raw-loader!./common/hashes/sha256/pad512ThenHash.zok';
+import pad640ThenHash from '!!raw-loader!./common/hashes/sha256/pad640ThenHash.zok';
+import pad768ThenHash from '!!raw-loader!./common/hashes/sha256/pad768ThenHash.zok';
+import pad896ThenHash from '!!raw-loader!./common/hashes/sha256/pad896ThenHash.zok';
+import pad1024ThenHash from '!!raw-loader!./common/hashes/sha256/pad1024ThenHash.zok';
+import pad1152ThenHash from '!!raw-loader!./common/hashes/sha256/pad1152ThenHash.zok';
+import pad1280ThenHash from '!!raw-loader!./common/hashes/sha256/pad1280ThenHash.zok';
+import pad1536ThenHash from '!!raw-loader!./common/hashes/sha256/pad1536ThenHash.zok';
+import pad1792ThenHash from '!!raw-loader!./common/hashes/sha256/pad1792ThenHash.zok';
+import pad3072ThenHash from '!!raw-loader!./common/hashes/sha256/pad3072ThenHash.zok';
+import pad3840ThenHash from '!!raw-loader!./common/hashes/sha256/pad3840ThenHash.zok';
+import pad4096ThenHash from '!!raw-loader!./common/hashes/sha256/pad4096ThenHash.zok';
+
+//hashToCurve
+import chi from '!!raw-loader!./common/hashToCurve/chi.zok';
+import elligator from '!!raw-loader!./common/hashToCurve/elligator2.zok';
+import constPowerModofhalfp from '!!raw-loader!./common/hashToCurve/constPowerModOfhalfp-1.zok';
+import montgomeryToTwistedEdwards from '!!raw-loader!./common/hashToCurve/montgomeryToTwistedEdwards.zok';
+
+//merkle-tree
+import pathCheck from '!!raw-loader!./common/merkle-tree/mimc-path-check.zok';
 
 export {
   deposit,
   depositStub,
+  depositNoHash,
   doubleTransfer,
   doubleTransferStub,
+  doubleTransferNoHash,
   singleTransfer,
   singleTransferStub,
+  singleTransferNoHash,
   withdraw,
   withdrawStub,
-  pad1280ThenHash,
-  pad1024ThenHash,
+  withdrawNoHash,
+  orderLeftRight,
+  elgamal,
+  mimcEncryption,
+  mimcConstants,
+  mimcHash1,
+  mimcHash2,
+  mimcHash5,
+  hash512,
+  hash1024,
   hash1536,
+  hash2048,
+  hash3584,
+  hash4096,
+  hash4608,
+  pad256ThenHash,
+  pad512ThenHash,
+  pad640ThenHash,
+  pad768ThenHash,
+  pad896ThenHash,
+  pad1024ThenHash,
+  pad1152ThenHash,
+  pad1280ThenHash,
+  pad1536ThenHash,
+  pad1792ThenHash,
+  pad3072ThenHash,
+  pad3840ThenHash,
+  pad4096ThenHash,
+  chi,
+  elligator,
+  constPowerModofhalfp,
+  montgomeryToTwistedEdwards,
+  pathCheck,
 };
+
---
+++ wallet/src/views/zokrates/withdraw.view.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { initialize } from 'zokrates-js';
+
+import { withdrawNoHash, mimcHash2, mimcEncryption, mimcConstants, pad512ThenHash, pad1536ThenHash, pad1280ThenHash, hash1024, hash2048, hash1536, pathCheck } from './circuits';
+
+export default function Zokrates() {
+  const [proof, setProof] = useState('');
+  useEffect(async () => {
+    initialize().then(zokratesProvider => {
+      // const source = "def main(private field a) -> (field): return a * a";
+      const source = withdrawNoHash;
+      const options = {
+        location: 'main.zok', // location of the root module
+        resolveCallback: (currentLocation, importLocation) => {
+          console.log(`${currentLocation} is importing ' ${importLocation}`);
+          let code;
+          if (importLocation.endsWith('mimc-hash-2.zok')) code = mimcHash2;
+          else if (importLocation.endsWith('mimc-encryption.zok')) code = mimcEncryption;
+          else if (importLocation.endsWith('mimc-constants.zok')) code = mimcConstants;
+          else if (importLocation.endsWith('pad512ThenHash.zok')) code = pad512ThenHash;
+          else if (importLocation.endsWith('pad1536ThenHash.zok')) code = pad1536ThenHash;
+          else if (importLocation.endsWith('pad1280ThenHash.zok')) code = pad1280ThenHash;
+          else if (importLocation.endsWith('hash1024')) code = hash1024;
+          else if (importLocation.endsWith('hash1536')) code = hash1536;
+          else if (importLocation.endsWith('hash2048')) code = hash2048;
+          else if (importLocation.endsWith('mimc-path-check.zok')) code = pathCheck;
+          return {
+            source: code,
+            location: importLocation,
+          };
+        },
+      };
+// 0 0 0 3786913876 4053413099 4184556347 2734706904 2298878123 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10 2219597431 1755426215 3293145557 3243256786 3816304020 1517647008 425275575 1318493102 366750885 2604783080 3623807610 4147206897 2198290470 1707590623 385867958 4233993206 20554508024765599754681452527149817852616693887653983104651939102134258746269 60165613 1853956399 2123184558 1150957582 2995252443 2268101302 4050509027 3488140304 625919956 422449450 1194504794 2151460778 3906881990 2763939802 883444624 2224534737 893705366084700132548040460931008653290460453987 6100509968277764863775768711148611107175898089815871963355773213731205366508 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 13951531544348673269400720537311387247715263843017456944493996457606504233631 0 20781082199163440865218068816821413366576335772482013527788056044402762718175 4
+
+
+
+        const ercAddress = ["0", "0", "0", "3786913876", "4053413099", "4184556347", "2734706904", "2298878123"];
+        const id = ["0", "0", "0", "0", "0", "0", "0", "0"];
+        const value = ["0", "0", "0", "0", "0", "0", "0", "10"];
+        const salt = ["2219597431", "1755426215", "3293145557", "3243256786", "3816304020", "1517647008", "425275575", "1318493102"];
+        const hash = ["366750885", "2604783080", "3623807610", "4147206897", "2198290470", "1707590623", "385867958", "4233993206"];
+        const ask = "20554508024765599754681452527149817852616693887653983104651939102134258746269";
+      
+      const oldCommitment = {ercAddress: ercAddress, id:id, value: value, salt: salt, hash: hash, ask:ask} 
+
+      const nsk = ["60165613", "1853956399", "2123184558", "1150957582", "2995252443", "2268101302", "4050509027", "3488140304"];
+      const hashnullifier = ["625919956", "422449450", "1194504794", "2151460778", "3906881990", "2763939802", "883444624", "2224534737"];
+
+      const nullifier = {nsk:nsk, hash:hashnullifier};
+
+      const recipientAddress = "893705366084700132548040460931008653290460453987";
+
+      const path = ["6100509968277764863775768711148611107175898089815871963355773213731205366508", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "13951531544348673269400720537311387247715263843017456944493996457606504233631", "0", "20781082199163440865218068816821413366576335772482013527788056044402762718175"];
+      const order = "4";
+
+
+      const artifacts = zokratesProvider.compile(source, options);
+      // computation
+      //const { witness, output } = zokratesProvider.computeWitness(artifacts, ['2']);
+      const { witness, output } = zokratesProvider.computeWitness(artifacts, [oldCommitment, nullifier, recipientAddress, path, order]);
+
+    //   console.log(output, typeof output);
+    //   // run setup
+      const keypair = zokratesProvider.setup(artifacts.program);
+      // generate proof
+      const genProof = zokratesProvider.generateProof(artifacts.program, witness, keypair.pk);
+      console.log(genProof);
+      // export solidity verifier
+      // const verifier = zokratesProvider.exportSolidityVerifier(keypair.vk, 'v1');
+      // console.log(verifier);
+       setProof(JSON.stringify(genProof, 2, 2));
+       if ( zokratesProvider.verify(keypair.vk, genProof)) {
+        console.log("Proof is correct...")
+       }
+    });
+  }, []);
+
+  return (
+    <div>
+      <span>withdraw work beigin</span>
+      <p>{proof}</p>
+    </div>
+  );
+}
