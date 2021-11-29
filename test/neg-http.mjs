fa2ed2ca784b6d8c6509bb4c7f5501baf9bf4b78
Liju Jose <liju.jose@gds.ey.com>
2021-11-29 07:35:18 +0530

    fix: #318 hosted geth config configured

--- test/neg-http.mjs
+++ test/neg-http.mjs
@@ -35,6 +35,7 @@ describe('Testing the challenge http API', () => {
   let compressedPkd1;
 
   const USE_INFURA = process.env.USE_INFURA === 'true';
+  const USE_HOSTED_GETH = process.env.USE_HOSTED_GETH === 'true';
   const { ETH_PRIVATE_KEY, BLOCKCHAIN_URL } = process.env;
 
   const url = 'http://localhost:8080';
@@ -86,7 +87,7 @@ describe('Testing the challenge http API', () => {
   before(async () => {
     web3 = await connectWeb3(BLOCKCHAIN_URL);
 
-    if (USE_INFURA) {
+    if (USE_INFURA || USE_HOSTED_GETH) {
       if (!ETH_PRIVATE_KEY) {
         throw Error(
           'Cannot use default private key, please set environment variable ETH_PRIVATE_KEY',
