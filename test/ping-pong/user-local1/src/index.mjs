09d21559f780a2c7137b05b7c715f42a9313bc5a
Chaitanya Konda <ckonda@uk.ey.com>
2021-12-07 11:33:41 +0000

    feat(ping pong test): working test

--- test/ping-pong/user-local1/src/index.mjs
+++ test/ping-pong/user-local1/src/index.mjs
@@ -3,16 +3,15 @@ Module that runs up as a user
 */
 
 /* eslint-disable no-await-in-loop */
-/* eslint no-constant-condition: ["error", { "checkLoops": false } ] */
 
-// import logger from 'common-files/utils/logger.mjs';
+import logger from 'common-files/utils/logger.mjs';
 import config from 'config';
 import Nf3 from '../../../../cli/lib/nf3.mjs';
 import { waitForSufficientBalance, retrieveL2Balance } from './utils.mjs';
 
 const {
   zkpMnemonic,
-  user1EthereumSigningKey,
+  userEthereumSigningKey,
   optimistWsUrl,
   web3WsUrl,
   clientBaseUrl,
@@ -20,15 +19,14 @@ const {
   TRANSACTIONS_PER_BLOCK,
 } = config;
 
-const TEST_LENGTH = 1;
+const { TEST_LENGTH, ERC20_NAME } = process.env;
 const recipientPkd = process.env.RECIPIENT_PKD.split(',');
 
 /**
 Does the preliminary setup and starts listening on the websocket
 */
 async function localTest() {
-  // logger.info('Starting local test...');
-  console.log('Starting local test...');
+  logger.info('Starting local test...');
 
   const tokenType = 'ERC20';
   const value = 1;
@@ -38,37 +36,40 @@ async function localTest() {
     optimistBaseUrl,
     optimistWsUrl,
     web3WsUrl,
-    user1EthereumSigningKey,
+    userEthereumSigningKey,
   );
 
   await nf3.init(zkpMnemonic);
-  if (await nf3.healthcheck('client')) console.log('Healthcheck passed');
+  if (await nf3.healthcheck('client')) logger.info('Healthcheck passed');
   else throw new Error('Healthcheck failed');
 
-  const ercAddress = await nf3.getContractAddress('ERC20Mock1');
+  const ercAddress = await nf3.getContractAddress(ERC20_NAME);
   const startBalance = await retrieveL2Balance(nf3);
-  console.log('HERE startBalance', startBalance);
 
-  // To start with create a block of deposits such that, we do a transfer in the next block
+  // Create a block of deposits
   for (let i = 0; i < TRANSACTIONS_PER_BLOCK; i++) {
     await nf3.deposit(ercAddress, tokenType, value, tokenId);
   }
 
+  // Create a block of transfer and deposit transactions
   for (let i = 0; i < TEST_LENGTH; i++) {
     await waitForSufficientBalance(nf3, value);
     await nf3.transfer(false, ercAddress, tokenType, value, tokenId, recipientPkd);
     await nf3.deposit(ercAddress, tokenType, value, tokenId);
-    await new Promise(resolve => setTimeout(resolve, 20000));
+    await new Promise(resolve => setTimeout(resolve, 10000));
   }
 
+  // Wait for sometime at the end to retrieve balance to include any transactions sent by the other use
+  await new Promise(resolving => setTimeout(resolving, 200000));
   const endBalance = await retrieveL2Balance(nf3);
-  console.log('HERE endBalance', endBalance);
 
-  if (endBalance - startBalance === 2 * value) {
-    console.log('Test passed');
+  if (endBalance - startBalance === 2 * value + value * TEST_LENGTH) {
+    logger.info('Test passed');
+    logger.info('Balance of User (2*value (2*1) + value received) ', endBalance - startBalance);
+    logger.info('Amount sent to other User', value * TEST_LENGTH);
     nf3.close();
   } else {
-    console.log('The test failed because the L2 balance has not increased');
+    logger.info('The test failed because the L2 balance has not increased');
     process.exit(1);
   }
 }
--- test/ping-pong/user-local1/src/utils.mjs
+++ test/ping-pong/user-local1/src/utils.mjs
@@ -26,28 +26,20 @@ export const waitForSufficientBalance = (client, value) => {
   return new Promise(resolve => {
     async function isSufficientBalance() {
       const balances = await client.getLayer2Balances();
-      // if layer 2 balances don't exist, then wait a bit and look for balances again
-      if (Object.keys(balances).length === 0) {
+      if (
+        Object.keys(balances).length === 0 ||
+        balances[client.zkpKeys.compressedPkd] === undefined ||
+        Object.keys(balances[client.zkpKeys.compressedPkd]).length === 0 ||
+        balances[client.zkpKeys.compressedPkd][
+          Object.keys(balances[client.zkpKeys.compressedPkd])[0]
+        ] === undefined ||
+        balances[client.zkpKeys.compressedPkd][
+          Object.keys(balances[client.zkpKeys.compressedPkd])[0]
+        ] < value
+      ) {
         await new Promise(resolving => setTimeout(resolving, 10000));
         isSufficientBalance();
-      }
-      // if client does not have layer 2 balances, then wait a bit and look again
-      const clientBalances = balances[client.zkpKeys.compressedPkd];
-      if (clientBalances === undefined || Object.keys(clientBalances).length === 0) {
-        await new Promise(resolving => setTimeout(resolving, 10000));
-        isSufficientBalance();
-      }
-      console.log('HERE clientBalances', clientBalances);
-      const balance = clientBalances[Object.keys(clientBalances)[0]];
-      // if client has layer 2 balances and if it is equal to value required
-      if (balance > value) {
-        // console.log('sufficient balance');
-        resolve();
-      } else {
-        // console.log('insufficient balance', balance);
-        await new Promise(resolving => setTimeout(resolving, 10000));
-        isSufficientBalance();
-      }
+      } else resolve();
     }
     isSufficientBalance();
   });
