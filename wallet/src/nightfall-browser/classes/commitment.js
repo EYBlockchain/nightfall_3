89ab88c3adbd0677594ec97cf1ab6429e7f3e0b5
Liju Jose <liju.jose@gds.ey.com>
2021-12-08 21:52:51 +0530

    fix: deposit work in progress

---
+++ wallet/src/nightfall-browser/classes/commitment.js
@@ -0,0 +1,52 @@
+/**
+A commitment class
+*/
+import gen from 'general-number';
+import sha256 from '../../common-files/utils/crypto/sha256';
+
+const { generalise } = gen;
+
+class Commitment {
+  preimage;
+
+  hash;
+
+  isNullified = false;
+
+  isNullifiedOnChain = -1;
+
+  constructor({ ercAddress, tokenId, value, pkd = [], compressedPkd, salt }) {
+    const items = { ercAddress, tokenId, value, pkd, compressedPkd, salt };
+    const keys = Object.keys(items);
+    for (const key of keys)
+      if (items[key] === undefined)
+        throw new Error(
+          `Property ${key} was undefined. Did you pass the wrong object to the constructor?`,
+        );
+    this.preimage = generalise({
+      ercAddress,
+      tokenId,
+      value,
+      pkd,
+      compressedPkd,
+      salt,
+    });
+    this.hash = sha256([
+      this.preimage.ercAddress,
+      this.preimage.tokenId,
+      this.preimage.value,
+      this.preimage.compressedPkd,
+      this.preimage.salt,
+    ]);
+  }
+
+  // sometimes (e.g. going over http) the general-number class is inconvenient
+  toHex() {
+    return {
+      preimage: this.preimage.all.hex(),
+      hash: this.hash.hex(),
+    };
+  }
+}
+
+export default Commitment;
