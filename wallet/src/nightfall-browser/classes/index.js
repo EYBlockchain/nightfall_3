04d4209c0ff4b03f232667cc647ab0b7683b7208
Liju Jose <liju.jose@gds.ey.com>
2021-12-08 21:52:51 +0530

    fix: deposit work in progress

---
+++ wallet/src/nightfall-browser/classes/index.js
@@ -0,0 +1,8 @@
+import PublicInputs from '../../common-files/classes/public-inputs';
+import Transaction from '../../common-files/classes/transaction';
+
+import Commitment from './commitment';
+import Nullifier from './nullifier';
+import Secrets from './secrets';
+
+export { PublicInputs, Transaction, Commitment, Nullifier, Secrets };
