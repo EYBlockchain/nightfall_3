9941bcbecc5adeef89dd1709cd47e0ee715ad362
Chaitanya Konda <ckonda@uk.ey.com>
2021-12-08 19:23:50 +0000

    feat: decompress key

--- nightfall-client/src/routes/generate-keys.mjs
+++ nightfall-client/src/routes/generate-keys.mjs
@@ -4,7 +4,8 @@ Route for transferring a crypto commitment.
 
 import express from 'express';
 import logger from 'common-files/utils/logger.mjs';
-import { generateKeys } from '../services/keys.mjs';
+import { generalise } from 'general-number';
+import { generateKeys, decompressKey } from '../services/keys.mjs';
 
 const router = express.Router();
 
@@ -16,6 +17,20 @@ router.post('/', async (req, res, next) => {
     logger.debug('returning keys');
     logger.silly(`the keys are ${JSON.stringify(keys, null, 2)}`);
     res.json(keys);
+  } catch (err) {
+    logger.error(err);
+    next(err);
+  }
+});
+
+router.post('/decompress', async (req, res, next) => {
+  logger.debug(`decompressing keys endpoint received POST ${JSON.stringify(req.body, null, 2)}`);
+  try {
+    const { compressedPkd } = generalise(req.body);
+    const decompressedPkd = await decompressKey(compressedPkd);
+    logger.debug('returning keys');
+    logger.silly(`the keys are ${JSON.stringify(decompressedPkd, null, 2)}`);
+    res.json({ pkd: [decompressedPkd[0].hex(), decompressedPkd[1].hex()] });
   } catch (err) {
     logger.error(err);
     next(err);
