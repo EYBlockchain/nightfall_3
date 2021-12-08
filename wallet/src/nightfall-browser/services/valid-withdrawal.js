11280bd70ff223fab97c6e878b9169c4d42cc9d0
Liju Jose <liju.jose@gds.ey.com>
2021-12-08 21:52:51 +0530

    fix: deposit work in progress

---
+++ wallet/src/nightfall-browser/services/valid-withdrawal.js
@@ -0,0 +1,28 @@
+/**
+Module to endable withdrawal of funds from the Shield contract to the user's
+address.
+*/
+import config from 'config';
+import { getContractInstance } from '../../common-files/utils/contract';
+import { Transaction } from '../classes/index';
+import { buildSolidityStruct } from './finalise-withdrawal';
+
+const { SHIELD_CONTRACT_NAME } = config;
+
+// eslint-disable-next-line import/prefer-default-export
+export async function isValidWithdrawal({ block, transactions, index }) {
+  const shieldContractInstance = await getContractInstance(SHIELD_CONTRACT_NAME);
+  try {
+    const valid = await shieldContractInstance.methods
+      .isValidWithdrawal(
+        buildSolidityStruct(block),
+        block.blockNumberL2,
+        transactions.map(t => Transaction.buildSolidityStruct(t)),
+        index,
+      )
+      .call();
+    return valid;
+  } catch (err) {
+    return false;
+  }
+}
