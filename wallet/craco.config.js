60fb859840e3e08d5a5e6dbea868e629e206f650
Liju Jose <liju.jose@gds.ey.com>
2021-12-12 15:45:54 +0530

    fix: successfully rebased

--- wallet/craco.config.js
+++ wallet/craco.config.js
@@ -1,13 +1,23 @@
 /* eslint-disable global-require */
 const { addBeforeLoader, loaderByName } = require('@craco/craco');
+// eslint-disable-next-line no-extend-native
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
 
 module.exports = {
   plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
   webpack: {
     configure: (webpackConfig, { paths }) => {
       const wasmExtensionRegExp = /\.wasm$/;
+      const appConfig = require('../config/default');
 
       webpackConfig.resolve.extensions.push('.wasm');
+
+      // eslint-disable-next-line no-param-reassign
+      webpackConfig.externals = webpackConfig.externals || {};
+      // eslint-disable-next-line no-param-reassign
+      webpackConfig.externals.config = JSON.stringify(appConfig);
 
       webpackConfig.module.rules.forEach(rule => {
         (rule.oneOf || []).forEach(oneOf => {
