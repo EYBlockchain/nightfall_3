0235acca67b730ba6759cc16a7fdbb694747c671
Westlad <Duncan.Westland@uk.ey.com>
2021-12-15 15:17:03 +0000

    fix: pull correct container images

--- pull-images.sh
+++ pull-images.sh
@@ -3,5 +3,5 @@ docker pull ghcr.io/eyblockchain/nightfall3-worker:latest
 docker pull ghcr.io/eyblockchain/nightfall3-client:latest
 docker pull ghcr.io/eyblockchain/nightfall3-optimist:latest
 docker pull ghcr.io/eyblockchain/nightfall3-proposer:latest
-docker pull ghcr.io/eyblockchain/nightfall3-user-local:latest
-
+docker pull ghcr.io/eyblockchain/nightfall3-user-local1:latest
+docker pull ghcr.io/eyblockchain/nightfall3-user-local2:latest
---
+++ push-images.sh
@@ -0,0 +1,7 @@
+docker push ghcr.io/eyblockchain/nightfall3-deployer:latest
+docker push ghcr.io/eyblockchain/nightfall3-worker:latest
+docker push ghcr.io/eyblockchain/nightfall3-client:latest
+docker push ghcr.io/eyblockchain/nightfall3-optimist:latest
+docker push ghcr.io/eyblockchain/nightfall3-proposer:latest
+docker push ghcr.io/eyblockchain/nightfall3-user-local1:latest
+docker push ghcr.io/eyblockchain/nightfall3-user-local2:latest
