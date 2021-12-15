cb95ef05c19ce41b29829db7e7f0bcc29995e4e8
David Ruiz <druiz0992@gmail.com>
2021-12-15 11:48:41 +0100

    fix: ongoing tests

--- wallet/test/wallet_test.py
+++ wallet/test/wallet_test.py
@@ -33,52 +33,60 @@ for arg in sys.argv:
     if arg.lower() == "ropsten":
         testEnvironment="ropsten"
 
-###################
-# Load selenium  #
-###################
-# Load chrome driver
-driver = initializeSelenium()
+#####################
+# Declare variables #
+#####################
 
-# Load find elements with the default wait
-defualtWaitSeconds = 5
-defaultWaitObject = WebDriverWait(driver, defualtWaitSeconds)
-findElementsInstance = findElements(defaultWaitObject)
+try:
+  ###################
+  # Load selenium  #
+  ###################
+  # Load chrome driver
+  driver = initializeSelenium()
+  
+  # Load find elements with the default wait
+  defualtWaitSeconds = 5
+  defaultWaitObject = WebDriverWait(driver, defualtWaitSeconds)
+  findElementsInstance = findElements(defaultWaitObject)
+  
+  # Identify the current tabs open
+  nightfallTab=driver.window_handles[1]
+  metamaskTab=driver.window_handles[0]
+  driver.switch_to.window(metamaskTab)
+  
+  # Configure variables depending on test environment
+  networkConfig = networkConfigLocalhost
+  walletUrl = walletUrlLocalhost
+  if testEnvironment == "docker":
+      networkConfig = networkConfigDocker
+      walletUrl = walletUrlDocker
+  elif testEnvironment == "ropsten":
+      networkConfig = networkConfigRopsten
+      walletUrl = walletUrlRopsten
+     
+  
+  ###################
+  # Load Metamask   #
+  ###################
+  initializeMetamask(driver, findElementsInstance, metamaskConfig)
+  #deleteNetworkMetamask(driver, findElementsInstance, deleteNetworkConfig)
+  #selectTestNetworkMetamask(driver, findElementsInstance, networkConfigRopsten)
+  selectNetworkMetamask(driver, findElementsInstance, networkConfig)
+  #addEthAccountMetamask(driver, findElementsInstance, ethAccount1Params)
+  addEthAccountMetamask(driver, findElementsInstance, ethAccount2Params)
+  
+  # Add tokens to metamask
+  #addTokenMetamask("0x4232AF76301fd6c2B144A7A5A7796331B2A43D90", findElementsInstance)
+  ########################
+  # Log in wallet #
+  ########################
+  loginNightfallWallet(driver, findElementsInstance, metamaskTab, nightfallTab, walletUrl)
+  testEthAddress = findElementsInstance.element_exist_xpath('//*[@id="wallet-info-cell-ethaddress"]').text
+  assert(testEthAddress.lower() == ethAccount2Params["ethereumAddress"].lower())
+except Exception:
+  print("FAILED")
 
-# Identify the current tabs open
-nightfallTab=driver.window_handles[1]
-metamaskTab=driver.window_handles[0]
-driver.switch_to.window(metamaskTab)
-
-# Configure variables depending on test environment
-networkConfig = networkConfigLocalhost
-walletUrl = walletUrlLocalhost
-if testEnvironment == "docker":
-    networkConfig = networkConfigDocker
-    walletUrl = walletUrlDocker
-elif testEnvironment == "ropsten":
-    networkConfig = networkConfigRopsten
-    walletUrl = walletUrlRopsten
-   
-
-###################
-# Load Metamask   #
-###################
-initializeMetamask(driver, findElementsInstance, metamaskConfig)
-#deleteNetworkMetamask(driver, findElementsInstance, deleteNetworkConfig)
-#selectTestNetworkMetamask(driver, findElementsInstance, networkConfigRopsten)
-selectNetworkMetamask(driver, findElementsInstance, networkConfig)
-#addEthAccountMetamask(driver, findElementsInstance, ethAccount1Params)
-addEthAccountMetamask(driver, findElementsInstance, ethAccount2Params)
-
-# Add tokens to metamask
-#addTokenMetamask("0x4232AF76301fd6c2B144A7A5A7796331B2A43D90", findElementsInstance)
-########################
-# Log in wallet #
-########################
-loginNightfallWallet(driver, findElementsInstance, metamaskTab, nightfallTab, walletUrl)
-testEthAddress = findElementsInstance.element_exist_xpath('//*[@id="wallet-info-cell-ethaddress"]').text
-assert(testEthAddress.lower() == ethAccount2Params["ethereumAddress"].lower())
-
+  
 ########################
 # Start Tests          
 ########################
