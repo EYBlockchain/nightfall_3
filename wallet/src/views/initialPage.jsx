7031d3612d26378875a775ec545da18e33f79ba5
Luiz <luiz.oamorim@gmail.com>
2022-01-19 10:00:54 -0300

    fix(eslint): fixing eslint problems

--- wallet/src/views/initialPage.jsx
+++ wallet/src/views/initialPage.jsx
@@ -1,81 +1,66 @@
-import styles from "../styles/initialPage.module.scss"
+import React from 'react';
+import styles from '../styles/initialPage.module.scss';
 
 const cardsData = [
   {
     imageName: 'poly-wallet',
     title: 'Polygon Wallet',
     desc: 'Send and receive crypto assets on Polygon network',
-    route: 'wallet',    
+    route: 'wallet',
   },
   {
     imageName: 'poly-nightfall-wallet',
     title: 'Polygon Nightfall Wallet',
     desc: 'Send and receive crypto assets on Polygon Nightfall network',
-    route: 'walletNF3',    
-  },  
+    route: 'walletNF3',
+  },
   {
     imageName: 'poly-bridge',
     title: 'Polygon Bridge',
     desc: 'Deposit and withdraw between networks',
-    route: 'bridge',    
+    route: 'bridge',
   },
   {
     imageName: 'poly-staking',
     title: 'Polygon Staking',
     desc: 'Stake MATIC to earn rewards',
     route: 'staking',
-    tag: 'Live on ETHEREUM chain',    
+    tag: 'Live on ETHEREUM chain',
   },
   {
     imageName: 'poly-widget-dashboard',
     title: 'Widget Dashboard',
     desc: 'Manage all your Polygon wallet widgets at one place',
     route: 'widget-dashboard',
-    tag: 'Supported only on desktop',    
+    tag: 'Supported only on desktop',
   },
-]
-
+];
+// src={require('../static/img/homepage/poly-bridge.png')}
 export default function MainPage() {
   return (
     <div className={styles.homepage}>
-      
-      <div className={styles.headerh2} >
-        Getting started with Polygon PoS chain
-      </div>
-      <div className="font-body-medium text-center text-gray-500 ms-t-12 ms-b-38" >
+      <div className={styles.headerh2}>Getting started with Polygon PoS chain</div>
+      <div className="font-body-medium text-center text-gray-500 ms-t-12 ms-b-38">
         The safe, fast, and most secure way to use Polygon PoS.
       </div>
       <a href="/login">
-        <div
-          className={styles.cardsContainer}
-        >        
-          {cardsData.map(card => (
-            <div className={styles.mcard}>
-              <div style={{marginTop: '20px', marginBottom: '15px'}}>
-              {/* TODO fix the .tag and width and height */}
-              <img        
-                width={105} height={110}
-                src={require(`../static/img/homepage/${card.imageName}.png`)}
-                alt={card.title}
-              />
+        <div className={styles.cardsContainer}>
+          {cardsData.map((card, index) => (
+            <div className={styles.mcard} key={index}>
+              <div style={{ marginTop: '20px', marginBottom: '15px' }}>
+                {/* TODO fix the .tag and width and height */}
+                <img src="" width={105} height={110} alt={card.title} />
               </div>
-              <div className={styles.headerh4}>
-                { card.title }
-              </div>
-              <div className="desc text-gray-500 ms-t-8">
-                { card.desc }
-              </div>
-              {card.tag &&
+              <div className={styles.headerh4}>{card.title}</div>
+              <div className="desc text-gray-500 ms-t-8">{card.desc}</div>
+              {card.tag && (
                 // <div v-if="data.tag" className="tag ms-t-44 d-inline-block"></div>
-                <div className={styles.tag}>
-                  { card.tag }
-                </div>
-              }
+                <div className={styles.tag}>{card.tag}</div>
+              )}
             </div>
           ))}
         </div>
       </a>
-    </div>  )
-  
+    </div>
+  );
 }
-
