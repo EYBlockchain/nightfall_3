4defe6c555180259232fc1cfc4e0f4cd4070a30b
Luiz <luiz.oamorim@gmail.com>
2022-01-20 10:54:57 -0300

    fix: problems with page redirections just adding a exec tag on the main route

--- wallet/src/views/initialPage.jsx
+++ wallet/src/views/initialPage.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import styles from '../styles/initialPage.module.scss';
+import polyNightfallWallet from '../static/img/homepage/polyNightfallWallet.png'
+import polyBridge from '../static/img/homepage/polyBridge.png'
+import polyStaking from '../static/img/homepage/polyStaking.png'
+import polyWallet from '../static/img/homepage/polyWallet.png'
+import polyWidgetDashboard from '../static/img/homepage/polyWidgetDashboard.png'
 
 const cardsData = [
   {
-    imageName: 'poly-wallet',
+    imageName: 'polyWallet',
     title: 'Polygon Wallet',
     desc: 'Send and receive crypto assets on Polygon network',
     route: 'wallet',
-  },
+  },  
   {
-    imageName: 'poly-nightfall-wallet',
-    title: 'Polygon Nightfall Wallet',
-    desc: 'Send and receive crypto assets on Polygon Nightfall network',
-    route: 'walletNF3',
-  },
-  {
-    imageName: 'poly-bridge',
+    imageName: 'polyBridge',
     title: 'Polygon Bridge',
     desc: 'Deposit and withdraw between networks',
     route: 'bridge',
   },
   {
-    imageName: 'poly-staking',
+    imageName: 'polyNightfallWallet',
+    title: 'Polygon Nightfall Wallet',
+    desc: 'Send and receive crypto assets on Polygon Nightfall network',
+    route: 'walletNF3',
+  },
+  {
+    imageName: 'polyStaking',
     title: 'Polygon Staking',
     desc: 'Stake MATIC to earn rewards',
     route: 'staking',
     tag: 'Live on ETHEREUM chain',
   },
   {
-    imageName: 'poly-widget-dashboard',
+    imageName: 'polyWidgetDashboard',
     title: 'Widget Dashboard',
     desc: 'Manage all your Polygon wallet widgets at one place',
     route: 'widget-dashboard',
@@ -42,25 +47,35 @@ export default function MainPage() {
       <div className={styles.headerh2}>Getting started with Polygon PoS chain</div>
       <div className="font-body-medium text-center text-gray-500 ms-t-12 ms-b-38">
         The safe, fast, and most secure way to use Polygon PoS.
-      </div>
-      <a href="/login">
+      </div>      
         <div className={styles.cardsContainer}>
           {cardsData.map((card, index) => (
-            <div className={styles.mcard} key={index}>
-              <div style={{ marginTop: '20px', marginBottom: '15px' }}>
-                {/* TODO fix the .tag and width and height */}
-                <img src="" width={105} height={110} alt={card.title} />
-              </div>
-              <div className={styles.headerh4}>{card.title}</div>
-              <div className="desc text-gray-500 ms-t-8">{card.desc}</div>
-              {card.tag && (
-                // <div v-if="data.tag" className="tag ms-t-44 d-inline-block"></div>
-                <div className={styles.tag}>{card.tag}</div>
-              )}
-            </div>
+            <a href={ card.imageName === 'polyNightfallWallet' && '/login' }>
+                <div className={styles.mcard} key={index}>
+                <div style={{ marginTop: '20px', marginBottom: '15px' }}>
+                    {/* TODO fix the .tag and width and height */}
+                    <img                         
+                        src={card.imageName === 'polyNightfallWallet' ? polyNightfallWallet 
+                            :card.imageName === 'polyBridge' ? polyBridge 
+                            :card.imageName === 'polyStaking' ? polyStaking
+                            :card.imageName === 'polyWallet' ? polyWallet
+                            :card.imageName === 'polyWidgetDashboard' && polyWidgetDashboard}
+                        width={105} 
+                        height={110} 
+                        alt={card.title} 
+                    />
+                </div>
+                <div className={styles.headerh4}>{card.title}</div>
+                <div className={styles.desc}>{card.desc}</div>
+                {card.tag && (
+                    // <div v-if="data.tag" className="tag ms-t-44 d-inline-block"></div>
+                    <div className={styles.tag}>{card.tag}</div>
+                )}
+                </div>
+            </a>
           ))}
         </div>
-      </a>
+      
     </div>
   );
 }
