e27886897daefa135789e1ca2c37083e5ba82f00
Westlad <Duncan.Westland@uk.ey.com>
2022-01-21 18:17:37 -0500

    fix: merge

--- nightfall-deployer/migrations/4_test_tokens_migration.js
+++ nightfall-deployer/migrations/4_test_tokens_migration.js
@@ -17,14 +17,17 @@ module.exports = function(deployer, _, accounts) {
     await deployer.deploy(ERC20Mock, 1001010000000000); // initialSupply
 
     const ERC20deployed = await ERC20Mock.deployed();
-    const ERC721deployed = await ERC721Mock.deployed();
-    const ERC1155deployed = await ERC1155Mock.deployed();
     // For ping pong tests
     for (const address of UserEthereumAddresses) {
       await ERC20deployed.transfer(address, 1000000);
     }
     if (!config.ETH_ADDRESS) {// indicates we're running a wallet test that uses hardcoded addresses
       // For e2e tests
+      await deployer.deploy(ERC721Mock);
+      await deployer.deploy(ERC1155Mock);
+      const ERC721deployed = await ERC721Mock.deployed();
+      const ERC1155deployed = await ERC1155Mock.deployed();
+
       for (let i=0; i < nERC721; i++){
         for (const address of UserEthereumAddresses) {
           await ERC721deployed.awardItem(address, `https://erc721mock/item-id-${i}.json`);
