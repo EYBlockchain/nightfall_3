a5a4f177313ffce3bb9da0e978592b6a9653ae4e
Ilyas Ridhuan <ilyasridhuan@gmail.com>
2022-01-22 16:14:06 +0000

    feat: web3 and nf3 instantiation

--- wallet/src/views/app.view.jsx
+++ wallet/src/views/app.view.jsx
@@ -16,6 +16,7 @@ import * as loginActions from '../store/login/login.actions';
 import MainPage from './initialPage.jsx';
 import Wallet from './wallet/index.jsx';
 import Bridge from './bridge/index.jsx';
+import { UserProvider } from '../hooks/User/index.js';
 
 function App({ onDeleteWallet }) {
   Nf3.Environment.setEnvironment(process.env.REACT_APP_ENVIRONMENT);
@@ -42,26 +43,28 @@ function App({ onDeleteWallet }) {
   }, [onDeleteWallet]);
 
   return (
-    <React.Fragment>
-      <Switch>
-        <Route path="/login" render={() => <Login />} />
-        <Route path="/" exact={true} render={() => <MainPage />} />
-        <Route path="/transactions" render={() => <Transactions />} />
-        <Route path="/wallet" render={() => <Wallet />} />
-        <Route path="/bridge" render={() => <Bridge />} />
-        <Route
-          path="/issues"
-          render={() => {
-            window.location = NF3_GITHUB_ISSUES_URL;
-          }}
-        />
-        <Route path="/deposit" render={() => <Deposit />} />
-        <Route path="/withdraw" render={() => <Withdraw />} />
-        <Route path="/singletransfer" render={() => <Singletransfer />} />
-        <Route path="/doubletransfer" render={() => <Doubletransfer />} />
-        <Redirect to="/" />
-      </Switch>
-    </React.Fragment>
+    <UserProvider>
+      <React.Fragment>
+        <Switch>
+          <Route path="/login" render={() => <Login />} />
+          <Route path="/" exact={true} render={() => <MainPage />} />
+          <Route path="/transactions" render={() => <Transactions />} />
+          <Route path="/wallet" render={() => <Wallet />} />
+          <Route path="/bridge" render={() => <Bridge />} />
+          <Route
+            path="/issues"
+            render={() => {
+              window.location = NF3_GITHUB_ISSUES_URL;
+            }}
+          />
+          <Route path="/deposit" render={() => <Deposit />} />
+          <Route path="/withdraw" render={() => <Withdraw />} />
+          <Route path="/singletransfer" render={() => <Singletransfer />} />
+          <Route path="/doubletransfer" render={() => <Doubletransfer />} />
+          <Redirect to="/" />
+        </Switch>
+      </React.Fragment>
+    </UserProvider>
   );
 }
 
