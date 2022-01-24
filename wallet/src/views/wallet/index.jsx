6d9b9530655b7848404c52c49c8e5086992dc409
Ilyas Ridhuan <ilyasridhuan@gmail.com>
2022-01-24 10:45:40 +0000

    feat: deposit and trfs in bridge with pop up

--- wallet/src/views/wallet/index.jsx
+++ wallet/src/views/wallet/index.jsx
@@ -5,6 +5,11 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import { generateMnemonic } from 'bip39';
 import * as Nf3 from 'nf3';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Assets from '../../components/Assets/index.jsx';
 import Tokens from '../../components/Tokens/index.jsx';
 import { getWalletBalance } from '../../nightfall-browser/services/commitment-storage.js';
@@ -25,10 +30,11 @@ const tokenMapping = {
     maticChainUsdBalance: '1.8',
     maticChainBalance: '10',
     symbol: 'MATIC',
+    tokenAddress: '0xf05e9fb485502e5a93990c714560b7ce654173c3',
   },
 };
 
-const compressedPkd = '0x9e989c1cdde6b046489665f71799783935c96574e94d85654c45b440c06b796d';
+// const compressedPkd = '0x9e989c1cdde6b046489665f71799783935c96574e94d85654c45b440c06b796d';
 const initialTokenState = [
   {
     maticChainUsdBalance: '0',
@@ -36,6 +42,7 @@ const initialTokenState = [
     name: 'ChainLink Token',
     symbol: 'LINK',
     order: 2,
+    tokenAddress: '0xf05e9fb485502e5a93990c714560b7ce654173c3',
   },
   {
     maticChainUsdBalance: '0',
@@ -43,6 +50,7 @@ const initialTokenState = [
     name: 'USDT',
     symbol: 'USDT',
     order: 2,
+    tokenAddress: '0xf05e9fb485502e5a93990c714560b7ce654173c3',
   },
   {
     maticChainUsdBalance: '0',
@@ -50,6 +58,7 @@ const initialTokenState = [
     name: 'Aave Token',
     symbol: 'AAVE',
     order: 2,
+    tokenAddress: '0xf05e9fb485502e5a93990c714560b7ce654173c3',
   },
   {
     maticChainUsdBalance: '0',
@@ -57,6 +66,7 @@ const initialTokenState = [
     name: 'Matic Token',
     symbol: 'MATIC',
     order: 1,
+    tokenAddress: '0xf05e9fb485502e5a93990c714560b7ce654173c3',
   },
 ];
 
@@ -66,31 +76,46 @@ This is a modal to detect if a wallet (mnemonic and passphrase) has been initial
 
 function WalletModal(props) {
   const [state, setState] = React.useContext(UserContext);
+  const [screenMnemonic, setScreenMnemonic] = React.useState();
   return (
     <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Modal heading</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Create a new Polygon Nightfall Wallet
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
+        <h4>Polygon Nightfall</h4>
         <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-          {state.mnemonic}
+          Polygon Nightfall accounts are protected by a 12 word mnemonic. It is important that you
+          securely store these words. Losing access to these words will result in a loss of access
+          to your funds. Do not reveal them to anyone.
         </p>
-        <Button
-          onClick={() =>
-            setState({
-              nf3: state.nf3,
-              walletInitialised: state.walletInitialised,
-              mnemonic: generateMnemonic(),
-              socket: state.socket,
-              zkpKeys: state.zkpKeys,
-            })
-          }
-        >
-          Generate Mnemonic
-        </Button>
+        <Container>
+          {[0, 1].map((r, key) => (
+            // eslint-disable-next-line react/jsx-key
+            <Row key={key}>
+              {[0, 1, 2, 3, 4, 5].map((c, innerKey) => (
+                // eslint-disable-next-line react/jsx-key
+                <Col key={innerKey}>
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      value={
+                        typeof screenMnemonic !== 'undefined'
+                          ? screenMnemonic.split(' ')[r * 5 + c]
+                          : ''
+                      }
+                      readOnly
+                      aria-label="Recipient's username"
+                      aria-describedby="basic-addon2"
+                    />
+                  </InputGroup>
+                </Col>
+              ))}
+            </Row>
+          ))}
+        </Container>
+        <Button onClick={() => setScreenMnemonic(generateMnemonic())}>Generate Mnemonic</Button>
       </Modal.Body>
       <Modal.Footer>
         <Button
@@ -100,10 +125,10 @@ function WalletModal(props) {
               state.nf3.ethereumAddress,
             );
             const hashedSignature = jsSha3.keccak256(signature);
-            Storage.mnemonicSet(state.nf3.ethereumAddress, state.mnemonic, hashedSignature);
+            Storage.mnemonicSet(state.nf3.ethereumAddress, screenMnemonic, hashedSignature);
             setState({
               nf3: state.nf3,
-              mnemonic: state.mnemonic,
+              mnemonic: screenMnemonic,
               walletInitialised: true,
               socket: state.socket,
               zkpKeys: state.zkpKeys,
@@ -130,10 +155,8 @@ export default function Wallet() {
       if (!state.nf3) {
         const nf3Env = Nf3.Environment.getCurrentEnvironment().currentEnvironment;
         const nf3 = await new Nf3.Nf3(nf3Env.web3WsUrl, '', nf3Env);
-        console.log('Preinit nf3', nf3);
         await nf3.init();
         Nf3.Environment.setContractAddresses(nf3);
-        console.log('nf3', nf3);
         setState({
           zkpKeys: state.zkpKeys,
           walletInitialised: state.walletInitialised,
@@ -147,39 +170,12 @@ export default function Wallet() {
   }, []);
 
   useEffect(() => {
-    console.log('State Socket', state);
-    if (!state.socket) {
-      const socket = new WebSocket('ws://localhost:8082');
-
-      // Connection opened
-      socket.addEventListener('open', function () {
-        console.log(`Websocket is open`);
-        socket.send('proposedBlock');
-      });
-
-      // Listen for messages
-      socket.addEventListener('message', async function (event) {
-        console.log('Message from server ', event.data);
-        await blockProposedEventHandler(JSON.parse(event.data));
-      });
-      setState({
-        zkpKeys: state.zkpKeys,
-        walletInitialised: state.walletInitialised,
-        nf3: state.nf3,
-        mnemonic: state.mnemonic,
-        socket,
-      });
-    }
-  });
-
-  useEffect(() => {
     async function checkWalletExists() {
       if (state.nf3) {
         const mnemonicExists = Storage.mnemonicGet(state.nf3.ethereumAddress);
         if (mnemonicExists) state.walletInitialised = true;
         if (state.walletInitialised) {
-          if (state.mnemonic) {
-            console.log('State Mnemonic', state.mnemonic);
+          if (typeof state.mnemonic !== 'undefined') {
             const zkpKeys = await generateKeys(
               state.mnemonic,
               `m/44'/60'/0'/${DEFAULT_NF_ADDRESS_INDEX.toString()}`,
@@ -205,21 +201,46 @@ export default function Wallet() {
             mnemonic: state.mnemonic,
             socket: state.socket,
           });
-          console.log('State After Wallet is created', state);
         } else setModalShow(true);
       } else console.log('NF3 is not setup');
     }
     checkWalletExists();
-  }, [state.nf3]);
+  }, [state.mnemonic, state.nf3]);
+
+  useEffect(() => {
+    if (typeof state.socket === 'undefined') {
+      const socket = new WebSocket('ws://localhost:8082');
+      // Connection opened
+      socket.addEventListener('open', function () {
+        console.log(`Websocket is open`);
+        socket.send('proposedBlock');
+      });
+
+      // Listen for messages
+      socket.addEventListener('message', async function (event) {
+        console.log('Message from server ', event.data);
+        await blockProposedEventHandler(JSON.parse(event.data));
+      });
+      setState({
+        zkpKeys: state.zkpKeys,
+        walletInitialised: state.walletInitialised,
+        nf3: state.nf3,
+        mnemonic: state.mnemonic,
+        socket,
+      });
+    }
+  });
 
   useEffect(() => {
     async function getL2Balance() {
       const l2Balance = await getWalletBalance();
-      console.log('L2Balance', l2Balance);
-      if (Object.keys(l2Balance).length !== 0) {
+      if (
+        Object.keys(l2Balance).length !== 0 &&
+        Object.prototype.hasOwnProperty.call(state, 'zkpKeys')
+      ) {
         // eslint-disable-next-line consistent-return, array-callback-return
         const updatedState = Object.keys(tokenMapping).map(t => {
-          const token = l2Balance[compressedPkd][t];
+          const token = l2Balance[state.zkpKeys.compressedPkd][t];
           const tokenInfo = tokenMapping[t];
           if (token) {
             const { maticChainBalance, ...rest } = tokenInfo;
@@ -235,11 +256,10 @@ export default function Wallet() {
           return i;
         });
         setTokens(newState.sort((a, b) => Number(a.order) - Number(b.order)));
-        console.log('tokens', tokens);
       }
     }
     getL2Balance();
-  }, []);
+  }, [state.zkpKeys]);
 
   return (
     <div className={styles.wallet}>
