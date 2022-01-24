49bfee1be774970980d3674e3b5641975b65b1c7
Luiz <luiz.oamorim@gmail.com>
2022-01-24 19:32:33 -0300

    feat: sidebar and navbar added to the project

--- wallet/src/views/wallet/index.jsx
+++ wallet/src/views/wallet/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Assets from '../../components/Assets/index.jsx';
+import Header from '../../components/Header/header.jsx';
+import SideBar from '../../components/SideBar/index.jsx';
 import Tokens from '../../components/Tokens/index.jsx';
 import styles from '../../styles/wallet.module.scss';
 
 export default function Wallet() {
   return (
     <div className={styles.wallet}>
-      <Assets />
-      <Tokens />
+      <Header />
+      <div className={styles.walletComponents}>
+        <div className={styles.walletComponents__left}>
+          <SideBar />
+        </div>
+        <div className={styles.walletComponents__right}>
+          <Assets />
+          <Tokens />
+        </div>
+      </div>
     </div>
   );
 }
