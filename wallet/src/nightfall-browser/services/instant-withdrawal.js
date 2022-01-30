83ea84ed26627232b415a8ad84a8c2a7e2875eb5
Liju Jose <liju.jose@gds.ey.com>
2022-01-30 22:51:43 +0530

    fix: abi added to nightfall-browser

--- wallet/src/nightfall-browser/services/instant-withdrawal.js
+++ wallet/src/nightfall-browser/services/instant-withdrawal.js
@@ -10,13 +10,16 @@ import { getTransactionByTransactionHash, getBlockByTransactionHash } from './da
 
 const { SHIELD_CONTRACT_NAME } = global.config;
 
-const setInstantWithdrawl = async ({ transactionHash }) => {
+const setInstantWithdrawl = async ({ transactionHash }, shieldContractAddress) => {
   const block = await getBlockByTransactionHash(transactionHash);
   const transactions = await Promise.all(
     block.transactionHashes.map(t => getTransactionByTransactionHash(t)),
   );
   const index = transactions.findIndex(f => f.transactionHash === transactionHash);
-  const shieldContractInstance = await getContractInstance(SHIELD_CONTRACT_NAME);
+  const shieldContractInstance = await getContractInstance(
+    SHIELD_CONTRACT_NAME,
+    shieldContractAddress,
+  );
   try {
     const rawTransaction = await shieldContractInstance.methods
       .setAdvanceWithdrawalFee(
