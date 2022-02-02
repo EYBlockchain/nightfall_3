85b39240721ba4fae15d655f52c4ff7ff7ce8165
Luiz <luiz.oamorim@gmail.com>
2022-02-02 07:33:31 -0300

    fix: eslint errors

--- wallet/src/views/initialPage.jsx
+++ wallet/src/views/initialPage.jsx
@@ -6,7 +6,7 @@ import polyStaking from '../static/img/homepage/poly-staking.png';
 import polyWallet from '../static/img/homepage/poly-wallet.png';
 import polyWidgetDashboard from '../static/img/homepage/poly-widget-dashboard.png';
 
-import './initial.scss'
+import './initial.scss';
 
 // fix: problems with page redirect and images presentation
 
@@ -57,39 +57,49 @@ export default function MainPage() {
   return (
     <div className="homepage">
       <div className="header-h2 text-center">Getting started with Polygon PoS chain</div>
-      <div className="font-body-medium text-center text-gray-500" style={{ marginTop: '12px', marginBottom: '38px' }}>      
+      <div
+        className="font-body-medium text-center text-gray-500"
+        style={{ marginTop: '12px', marginBottom: '38px' }}
+      >
         The safe, fast, and most secure way to use Polygon PoS.
       </div>
-      <div className="cards-container d-flex flex-column flex-lg-row justify-content-center"
+      <div
+        className="cards-container d-flex flex-column flex-lg-row justify-content-center"
         style={{ marginTop: '44px' }}
       >
         {cardsData.map((card, index) => (
-          <Link className={`m-card text-center ${card.imageName.toString()}`} key={index} to="/wallet">
-                     
-                {/* TODO fix the .tag and width and height */}
-                <img
-                  src={
-                    // eslint-disable-line no-nested-ternary
-                    card.imageName === 'poly-nightfall-wallet' // eslint-disable-line no-nested-ternary
-                      ? polyWallet // eslint-disable-line no-nested-ternary
-                      : card.imageName === 'poly-bridge' // eslint-disable-line no-nested-ternary
-                      ? polyBridge // eslint-disable-line no-nested-ternary
-                      : card.imageName === 'poly-staking' // eslint-disable-line no-nested-ternary
-                      ? polyStaking // eslint-disable-line no-nested-ternary
-                      : card.imageName === 'poly-wallet' // eslint-disable-line no-nested-ternary
-                      ? polyWallet // eslint-disable-line no-nested-ternary
-                      : card.imageName === 'poly-widget-dashboard' && polyWidgetDashboard // eslint-disable-line no-nested-ternary
-                  }                
-                  alt={card.title}
-                />
-              
-              <div className="header-h4 dark-700">{card.title}</div>
-              <div className="desc text-gray-500" style={{ marginTop: '8px' }}>{card.desc}</div>
-              {card.tag && (
-                // <div v-if="data.tag" className="tag ms-t-44 d-inline-block"></div>
-                <div className="tag d-inline-block" style={{ marginTop: '44px' }}>{card.tag}</div>
-              )}
-            
+          <Link
+            className={`m-card text-center ${card.imageName.toString()}`}
+            key={index}
+            to="/wallet"
+          >
+            {/* TODO fix the .tag and width and height */}
+            <img
+              src={
+                // eslint-disable-line no-nested-ternary
+                card.imageName === 'poly-nightfall-wallet' // eslint-disable-line no-nested-ternary
+                  ? polyWallet // eslint-disable-line no-nested-ternary
+                  : card.imageName === 'poly-bridge' // eslint-disable-line no-nested-ternary
+                  ? polyBridge // eslint-disable-line no-nested-ternary
+                  : card.imageName === 'poly-staking' // eslint-disable-line no-nested-ternary
+                  ? polyStaking // eslint-disable-line no-nested-ternary
+                  : card.imageName === 'poly-wallet' // eslint-disable-line no-nested-ternary
+                  ? polyWallet // eslint-disable-line no-nested-ternary
+                  : card.imageName === 'poly-widget-dashboard' && polyWidgetDashboard // eslint-disable-line no-nested-ternary
+              }
+              alt={card.title}
+            />
+
+            <div className="header-h4 dark-700">{card.title}</div>
+            <div className="desc text-gray-500" style={{ marginTop: '8px' }}>
+              {card.desc}
+            </div>
+            {card.tag && (
+              // <div v-if="data.tag" className="tag ms-t-44 d-inline-block"></div>
+              <div className="tag d-inline-block" style={{ marginTop: '44px' }}>
+                {card.tag}
+              </div>
+            )}
           </Link>
         ))}
       </div>
