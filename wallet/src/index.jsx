d49114cdabbaf9b163a43d7692f2b5c2b7b52b97
Ilyas Ridhuan <ilyas.i.ridhuan@uk.ey.com>
2022-02-03 21:34:20 +0000

    chore: delete unused files (#458)

--- wallet/src/index.jsx
+++ wallet/src/index.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
 import './utils/parseConfigs';
-import configureStore from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
-import 'semantic-ui-less/semantic.less';
 import './index.css';
 import './views/app.style.css';
 import App from './views/app.view.jsx';
 import reportWebVitals from './reportWebVitals';
 
-const store = configureStore();
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <div>
-          <App />
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <div>
+        <App />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
