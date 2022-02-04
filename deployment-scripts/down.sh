c5022618347b8e024fa6ae0001de8e231f559dd7
Westlad <Duncan.Westland@uk.ey.com>
2022-02-04 15:09:04 +0000

    fix: check volume exists before deleting

--- deployment-scripts/down.sh
+++ deployment-scripts/down.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 # brings all containers down, deletes all volumes except for the trusted setup
+VOLUME_LIST=$(docker volume ls -q)
 
 docker-compose -p nightfall down --remove-orphans
-docker volume rm nightfall_build
+if [[ $(echo $VOLUME_LIST | grep nightfall_build) ]]; then
+  docker volume rm nightfall_build
+fi
