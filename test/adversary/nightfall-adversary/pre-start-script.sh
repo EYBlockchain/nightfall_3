470a7cc4a69678aee12e4323233f091a196fa6d5
Chaitanya Konda <ckonda@uk.ey.com>
2022-02-14 15:32:19 +0000

    fix: adding nightfall-adversary to gitignore

--- test/adversary/nightfall-adversary/pre-start-script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=../common-files/node_modules
-if [[ -d "$DIR" ]]; then
-  rm -dr ../common-files/node_modules
-fi
-
-rm ./node_modules/common-files
-cp -R ../common-files ./node_modules
--- test/adversary/nightfall-adversary/src/routes/index.mjs
+++
@@ -1,7 +0,0 @@
-import proposer from './proposer.mjs';
-import block from './block.mjs';
-import challenger from './challenger.mjs';
-import transaction from './transaction.mjs';
-import getContractAddress from './contract-address.mjs';
-
-export { proposer, block, challenger, transaction, getContractAddress };
