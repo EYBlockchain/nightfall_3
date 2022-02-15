4f99a26dbf8ba1a7e3e9b61b3a5ecfd558f9b713
Liju Jose <liju.jose@gds.ey.com>
2022-02-15 12:22:37 +0530

    test: 48

---
+++ wallet/tests/e2e/specs/e2e-spec.js
@@ -0,0 +1,49 @@
+describe('End to End tests', () => {
+  context('MetaMask', () => {
+    it('getNetwork should return network by default', () => {
+      cy.getNetwork().then(network => {
+        expect(network.networkName).to.be.equal('ganache-nightfall');
+        expect(network.networkId).to.be.equal(1337);
+      });
+    });
+
+    it('getMetamaskWalletAddress should return wallet address of current metamask account', () => {
+      cy.getMetamaskWalletAddress().then(address => {
+        expect(address).to.be.equal('0x9C8B2276D490141Ae1440Da660E470E7C0349C63');
+      });
+    });
+
+    it('acceptMetamaskAccess should accept connection request to metamask', () => {
+      cy.visit('/');
+      cy.acceptMetamaskAccess().then(connected => expect(connected).to.be.true);
+    });
+
+    it('generate Mnemonic and set state', () => {
+      cy.visit('/wallet');
+      cy.get('#generateMnemonic').contains('Generate Mnemonic');
+      cy.get('#generateMnemonic').click();
+      cy.get('#createWallet').click();
+      cy.confirmMetamaskSignatureRequest().then(confirmed => expect(confirmed).to.be.true);
+      cy.get('#TokenItem_tokenDepositMATIC').click();
+      cy.url().should('include', '/bridge');
+      cy.get('#Nightfall Assets').click();
+      cy.get('#generateMnemonic').should('not.exist');
+    });
+  });
+  context('Deposit', () => {
+    it('initial deposit with approve', () => {
+      // cy.visit('/wallet');
+      // cy.confirmMetamaskSignatureRequest().then(confirmed => expect(confirmed).to.be.true);
+      cy.get('#TokenItem_tokenDepositMATIC').click();
+      cy.url().should('include', '/bridge');
+      cy.get('#Bridge_amountDetails_tokenAmount').type(4)
+      cy.contains('Transfer').click();
+      cy.confirmMetamaskPermissionToSpend().then(approved => {
+        expect(approved).to.be.true;
+      });
+      cy.confirmMetamaskTransaction().then(confirmed => {
+        expect(confirmed).to.be.true;
+      });
+    });
+  });
+});
