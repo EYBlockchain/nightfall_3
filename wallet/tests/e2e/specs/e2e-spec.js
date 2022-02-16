ce52cdc6ea7f4de3c450d909e475f238c1cde11f
Liju Jose <liju.jose@gds.ey.com>
2022-02-16 07:43:28 +0530

    test: 61

--- wallet/tests/e2e/specs/e2e-spec.js
+++ wallet/tests/e2e/specs/e2e-spec.js
@@ -40,7 +40,7 @@ describe('End to End tests', () => {
       cy.get('button').contains('Transfer').click();
       cy.get('button').contains('Create Transaction').click();
       cy.get('#Bridge_modal_continueTransferButton').click();
-      cy.wait(10000);
+      cy.wait(20000);
       cy.confirmMetamaskPermissionToSpend().then(approved => expect(approved).to.be.true);
       cy.wait(10000);
       cy.confirmMetamaskTransaction().then(confirmed => expect(confirmed).to.be.true);
@@ -49,17 +49,19 @@ describe('End to End tests', () => {
     });
 
     it('second deposit which will create a new block', () => {
+      cy.wait(10000);
       cy.url().should('include', '/bridge');
       cy.get('#Bridge_amountDetails_tokenAmount').clear().type(4);
       cy.get('button').contains('Transfer').click();
       cy.get('button').contains('Create Transaction').click();
       cy.get('#Bridge_modal_continueTransferButton').click();
-      cy.wait(10000);
+      cy.wait(20000);
       cy.confirmMetamaskTransaction().then(confirmed => expect(confirmed).to.be.true);
       cy.wait(10000);
       cy.get('.btn-close').click();
       cy.contains('Nightfall Assets').click();
       cy.url().should('include', '/wallet');
+      cy.wait(20000);
       cy.get('#TokenItem_tokenBalanceMATIC').contains('8.0011');
     });
   });
