4449e93c40d15d10d2194c44d733ba5d86c83bc6
Liju Jose <liju.jose@gds.ey.com>
2022-02-25 09:57:02 +0530

    test: 4.7

--- wallet/tests/e2e/specs/e2e-spec.js
+++ wallet/tests/e2e/specs/e2e-spec.js
@@ -207,8 +207,6 @@ describe('End to End tests', () => {
     it(`check token balance after transfer - before block proposed event`, () => {
       cy.get('#TokenItem_tokenBalanceMATIC').should($div => {
         const totalBalance = Number($div.text());
-        cy.log(currentTokenBalance);
-        cy.log(totalBalance);
         expect(totalBalance).to.equal(currentTokenBalance - depositValue * 2);
         currentTokenBalance = totalBalance;
       });
