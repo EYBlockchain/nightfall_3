21d5600fa3648226d5f5475242a709e533d5284a
Westlad <Duncan.Westland@uk.ey.com>
2022-02-28 17:37:36 +0000

    feat: add logging and registration check

--- test/ping-pong/proposer/src/index.mjs
+++ test/ping-pong/proposer/src/index.mjs
@@ -2,7 +2,7 @@
 Module that runs up as a proposer
 */
 import config from 'config';
-import logger from 'common-files/utils/logger.mjs';
+import logger from '../../../../common-files/utils/logger.mjs';
 import Nf3 from '../../../../cli/lib/nf3.mjs';
 
 const { proposerEthereumSigningKey, optimistWsUrl, web3WsUrl, optimistBaseUrl } = config;
@@ -20,8 +20,17 @@ async function startProposer() {
   await nf3.init(undefined, 'optimist');
   if (await nf3.healthcheck('optimist')) logger.info('Healthcheck passed');
   else throw new Error('Healthcheck failed');
-  await nf3.registerProposer();
-  logger.debug('Proposer registration complete');
+  logger.info('Attempting to register proposer');
+  // let's see if the proposer has been registered before
+  const { proposers } = await nf3.getProposers();
+  // if not, let's register them
+  if (proposers.length === 0) {
+    await nf3.registerProposer();
+    logger.info('Proposer registration complete');
+  } else if (!proposers.map(p => p.thisAddress).includes(nf3.ethereumAddress)) {
+    await nf3.registerProposer();
+    logger.info('Proposer registration complete');
+  } else logger.warn('Proposer appears to be registerd already');
   // TODO subscribe to layer 1 blocks and call change proposer
   nf3.startProposer();
   logger.info('Listening for incoming events');
