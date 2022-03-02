703541fbaa6d22f84953dc0de228d21cb71b93d0
David <druiz0992@gmail.com>
2022-03-02 11:13:24 +0100

    fix: add documentDb functionality

---
+++ common-files/utils/aws-secrets.mjs
@@ -0,0 +1,21 @@
+import AWS from 'aws-sdk';
+
+const { REGION, ACCESS_KEY_ID, SECRET_ACCESS_KEY } = process.env
+
+async function getAwsParameter(parameter, encryption) {
+  // Create a Secrets Manager client
+  const ssm = new AWS.SSM({
+    region: REGION,
+    accessKeyId : AWS_ACCESS_KEY_ID,
+    secretAccessKey : AWS_SECRET_ACCESS_KEY,
+  });
+  const params = {
+    Name: parameter,
+    WithDecryption: encryption,
+  };
+
+  const data = await ssm.getParameter(params).promise();
+  return data.Parameter.Value;
+}
+
+export default getAwsParameter;
