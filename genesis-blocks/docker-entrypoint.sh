3346e2bcd19eadc45a69e4edf2a7cfdeb0c6da69
daveroga <daveroga@gmail.com>
2022-03-05 01:48:04 +0100

    fix: start nightfall deployer-first-blocks

--- genesis-blocks/docker-entrypoint.sh
+++ genesis-blocks/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 set -o errexit
 set -o pipefail
-DEPLOYER_HOST=genesis-blocks_deployer_1
+
 # wait until there's a optimist instance up
 while ! nc -z ${CLIENT_HOST:-client} ${CLIENT_PORT:-80}; do sleep 3; done
 while true; do 
-  if ping -c 1 -W 1 "$DEPLOYER_HOST"; then
-    echo "$DEPLOYER_HOST is alive";
+  if ping -c 1 -W 1 "${DEPLOYER_HOST:-deployer}" > /dev/null 2>&1; then
+    #echo "${DEPLOYER_HOST:-deployer} is alive";
     sleep 3;
   else
-    echo "$DEPLOYER_HOST has finished";
+    #echo "${DEPLOYER_HOST:-deployer} has finished";
     break;
   fi
 done
