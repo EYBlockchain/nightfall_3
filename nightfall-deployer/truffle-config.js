1352c400c28dc395bbbe13f9c34401146ecff6f5
David <druiz0992@gmail.com>
2022-03-20 09:26:57 +0100

    fix: add goerli network

--- nightfall-deployer/truffle-config.js
+++ nightfall-deployer/truffle-config.js
@@ -103,6 +103,16 @@ module.exports = {
       gas: 7000000,
     },
 
+    goerli: {
+      provider: () => new HDWalletProvider(config.ETH_PRIVATE_KEY, config.BLOCKCHAIN_URL),
+      network_id: 5,
+      networkCheckTimeout: 1000000000,
+      timeoutBlocks: 2000,
+      skipDryRun: true,
+      websockets: true,
+      gasPrice: config.WEB3_OPTIONS.gasPrice,
+      gas: 7000000,
+    },
     mainnet: {
       provider: () => new HDWalletProvider(config.ETH_PRIVATE_KEY, config.BLOCKCHAIN_URL),
       network_id: 1,
