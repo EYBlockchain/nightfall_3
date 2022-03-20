2011b7c59acab88636d9b25dea6589e965ff8be8
David <druiz0992@gmail.com>
2022-03-20 20:26:00 +0100

    fix: deploy tokens enable

--- nightfall-deployer/migrations/3_test_tokens_migration.js
+++ nightfall-deployer/migrations/3_test_tokens_migration.js
@@ -6,6 +6,7 @@ const {
     addresses,
   },
 } = config;
+const { DEPLOY_MOCK_TOKENS = true } = process.env;
 
 const Shield = artifacts.require('Shield.sol');
 
@@ -18,11 +19,12 @@ const nERC721 = 35;
 
 module.exports = function (deployer, _, accounts) {
   deployer.then(async () => {
+    const restrictions = await Shield.deployed();
+
+    if (!DEPLOY_MOCK_TOKENS) return;
     await deployer.deploy(ERC20Mock, 1001010000000000); // initialSupply
 
-    const restrictions = await Shield.deployed();
     const ERC20deployed = await ERC20Mock.deployed();
-
     // For ping pong tests
     await ERC20deployed.transfer(addresses.user1, 1000000000000);
     await ERC20deployed.transfer(addresses.user2, 1000000000000);
