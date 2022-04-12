fc91d46a0f1f66e09883abb83e467170a1f3e2bb
Luiz <luiz.oamorim@gmail.com>
2022-04-12 19:40:20 -0300

    fix: removing unused files

--- wallet/src/components/BridgeComponent/utils/verifyIfValueIsGreaterThen.ts
+++
@@ -1,8 +0,0 @@
-const verifyIfValueIsGreaterThen = (inputAmount: number, accountBalance: number): boolean => {
-  if (inputAmount > accountBalance) {
-    return true;
-  }
-  return false;
-};
-
-export default verifyIfValueIsGreaterThen;
--- wallet/src/components/BridgeComponent/utils/verifyIfValueIsNegative.ts
+++
@@ -1,8 +0,0 @@
-const verifyIfValueIsNegative = (inputAmount: number): boolean => {
-  if (inputAmount < 0) {
-    return true;
-  }
-  return false;
-};
-
-export default verifyIfValueIsNegative;
