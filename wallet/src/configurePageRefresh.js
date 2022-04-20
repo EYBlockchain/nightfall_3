0616d0802be842dbd6f56d84fe85abd48a15dd90
Liju Jose <liju.jose@gds.ey.com>
2022-04-20 17:12:32 +0530

    fix: refresh bug fixed

--- wallet/src/configurePageRefresh.js
+++ wallet/src/configurePageRefresh.js
@@ -3,7 +3,7 @@ global.sharedWorkerpool = new Set();
 // warns at page reload if sharedWorker for generateProof
 // is in progress
 global.onbeforeunload = event => {
-  if (!global.sharedWorkerpool.size) return '';
+  if (!global.sharedWorkerpool.size) return null;
   const e = event || window.event;
   e.preventDefault();
   if (e) e.returnValue = '';
