a41791e116dcc3e1bcbe62b7b1a5af540425f1c1
daveroga <daveroga@gmail.com>
2022-04-20 12:52:10 +0200

    fix: ping pong test had undefined variables

--- test/ping-pong/user-local/src/index.mjs
+++ test/ping-pong/user-local/src/index.mjs
@@ -11,9 +11,17 @@ import { waitForSufficientBalance, retrieveL2Balance } from './utils.mjs';
 
 const environment = config.ENVIRONMENTS[process.env.ENVIRONMENT] || config.ENVIRONMENTS.localhost;
 
-const { mnemonics, signingKeys, txPerBlock, recipients } = config.TEST_OPTIONS;
+const { mnemonics, signingKeys, txPerBlock, pkds } = config.TEST_OPTIONS;
 
-const { TEST_LENGTH, ERC20_NAME, TX_WAIT = 1000, IS_TEST_RUNNER = '' } = process.env;
+const {
+  TEST_LENGTH,
+  ERC20_NAME,
+  TX_WAIT = 1000,
+  IS_TEST_RUNNER = '',
+  USER_ETHEREUM_SIGNING_KEY,
+  RECIPIENT_PKD,
+  ZKP_MNEMONIC,
+} = process.env;
 
 /**
 Does the preliminary setup and starts listening on the websocket
@@ -25,9 +33,13 @@ async function localTest() {
   const tokenType = 'ERC20';
   const value = 1;
   const tokenId = '0x0000000000000000000000000000000000000000000000000000000000000000';
-  const nf3 = new Nf3(IS_TEST_RUNNER ? signingKeys.user1 : signingKeys.user2, environment);
+  const signingKey =
+    USER_ETHEREUM_SIGNING_KEY || (IS_TEST_RUNNER ? signingKeys.user1 : signingKeys.user2);
+  const mnemonic = ZKP_MNEMONIC || (IS_TEST_RUNNER ? mnemonics.user1 : mnemonics.user2);
+  const nf3 = new Nf3(signingKey, environment);
+  const recipientPkd = RECIPIENT_PKD || pkds.user1;
 
-  await nf3.init(IS_TEST_RUNNER ? mnemonics.user1 : mnemonics.user2);
+  await nf3.init(mnemonic);
   if (await nf3.healthcheck('client')) logger.info('Healthcheck passed');
   else throw new Error('Healthcheck failed');
 
@@ -44,7 +56,7 @@ async function localTest() {
   for (let i = 0; i < TEST_LENGTH; i++) {
     await waitForSufficientBalance(nf3, value);
     try {
-      await nf3.transfer(offchainTx, ercAddress, tokenType, value, tokenId, recipients.user1);
+      await nf3.transfer(offchainTx, ercAddress, tokenType, value, tokenId, recipientPkd);
     } catch (err) {
       if (err.message.includes('No suitable commitments')) {
         // if we get here, it's possible that a block we are waiting for has not been proposed yet
@@ -55,7 +67,7 @@ async function localTest() {
           } seconds and try one last time`,
         );
         await new Promise(resolve => setTimeout(resolve, 10 * TX_WAIT));
-        await nf3.transfer(offchainTx, ercAddress, tokenType, value, tokenId, recipients.user1);
+        await nf3.transfer(offchainTx, ercAddress, tokenType, value, tokenId, recipientPkd);
       }
     }
     await nf3.deposit(ercAddress, tokenType, value, tokenId);
@@ -71,9 +83,12 @@ async function localTest() {
   if (IS_TEST_RUNNER) loopMax = 10; // the TEST_RUNNER must finish first so that its exit status is returned to the tester
   do {
     const endBalance = await retrieveL2Balance(nf3);
-    if (endBalance - startBalance === 2 * value + value * TEST_LENGTH && IS_TEST_RUNNER) {
+    if (endBalance - startBalance === txPerBlock * value + value * TEST_LENGTH && IS_TEST_RUNNER) {
       logger.info('Test passed');
-      logger.info('Balance of User (2*value (2*1) + value received) ', endBalance - startBalance);
+      logger.info(
+        'Balance of User (txPerBlock*value (txPerBlock*1) + value received) ',
+        endBalance - startBalance,
+      );
       logger.info('Amount sent to other User', value * TEST_LENGTH);
       nf3.close();
       process.exit(0);
@@ -81,7 +96,7 @@ async function localTest() {
       logger.info(
         'The test has not yet passed because the L2 balance has not increased, or I am not the test runner - waiting',
         endBalance - startBalance,
-        2 * value + value * TEST_LENGTH,
+        txPerBlock * value + value * TEST_LENGTH,
       );
       await new Promise(resolving => setTimeout(resolving, 20 * TX_WAIT)); // TODO get balance waiting working well
       loop++;
