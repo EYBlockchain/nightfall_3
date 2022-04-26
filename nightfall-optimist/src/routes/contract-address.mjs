302bc2f6257a9e506b055db0b08af26869226a21
Jose Pedro Sousa <github@zepedro.me>
2022-04-26 16:02:59 +0100

    feat: adding feebook implementation for browser 📖

--- nightfall-optimist/src/routes/contract-address.mjs
+++ nightfall-optimist/src/routes/contract-address.mjs
@@ -3,25 +3,23 @@ Route for depositing (minting) a crypto commitment.
 This code assumes that the Shield contract already has approval to spend
 funds on a zkp deposit
 */
-import express from "express";
-import logger from "common-files/utils/logger.mjs";
-import { getContractAddress } from "common-files/utils/contract.mjs";
+import express from 'express';
+import logger from 'common-files/utils/logger.mjs';
+import { getContractAddress } from 'common-files/utils/contract.mjs';
 
 const router = express.Router();
 
-router.get("/:contract", async (req, res, next) => {
-	logger.debug("contract-address endpoint received GET");
-	const { contract } = req.params;
-	try {
-		const address = await getContractAddress(contract);
-		logger.debug(
-			`returning address ${address.address} for contract ${address.contractName}`
-		);
-		res.json(address);
-	} catch (err) {
-		logger.error(err);
-		next(err);
-	}
+router.get('/:contract', async (req, res, next) => {
+  logger.debug('contract-address endpoint received GET');
+  const { contract } = req.params;
+  try {
+    const address = await getContractAddress(contract);
+    logger.debug(`returning address ${address.address} for contract ${address.contractName}`);
+    res.json(address);
+  } catch (err) {
+    logger.error(err);
+    next(err);
+  }
 });
 
 export default router;
