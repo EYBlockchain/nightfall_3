fb581ae2839b8ca10dd97a29a74c8e538f22356f
daveroga <daveroga@gmail.com>
2022-04-29 10:28:54 +0200

    fix: upgradable for aws infra

--- nightfall-deployer/entrypoint.sh
+++ nightfall-deployer/entrypoint.sh
@@ -7,10 +7,14 @@ if [ -z "${ETH_PRIVATE_KEY}" ]; then
   while ! nc -z ${BLOCKCHAIN_WS_HOST} ${BLOCKCHAIN_PORT}; do sleep 3; done
 fi
 npx truffle compile --all
-npx truffle migrate --to 3 --network=${ETH_NETWORK}
 
-if [ -n "${UPGRADE}" ]; then
-  npx truffle migrate -f 4 --reset --network=${ETH_NETWORK}
+if [ -z "${UPGRADE}" ]; then
+  echo 'Deploying contracts'
+  npx truffle migrate --to 3 --network=${ETH_NETWORK}
+  npx truffle migrate --f 5 --to 5 --network=${MATIC_NETWORK}
+else
+  echo 'Upgrading contracts'
+  npx truffle migrate -f 4 --network=${ETH_NETWORK} --skip-dry-run
 fi
 
 #sleep 10
