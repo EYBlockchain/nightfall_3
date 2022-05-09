16e5e2325f79db9b930430d06aaae5751e3d8dee
Westlad <Duncan.Westland@uk.ey.com>
2022-05-09 17:15:52 +0100

    fix: offchain transactions not seen before should not be put in mempool

--- nightfall-optimist/src/event-handlers/transaction-submitted.mjs
+++ nightfall-optimist/src/event-handlers/transaction-submitted.mjs
@@ -41,7 +41,7 @@ async function checkAlreadyInBlock(_transaction) {
 /**
 This handler runs whenever a new transaction is submitted to the blockchain
 */
-async function transactionSubmittedEventHandler(eventParams, mempool = true) {
+async function transactionSubmittedEventHandler(eventParams, mempool) {
   const { offchain = false, ...data } = eventParams;
   let transaction;
   if (offchain) {
@@ -72,7 +72,8 @@ async function transactionSubmittedEventHandler(eventParams, mempool = true) {
     }
     if (transactionNullifiers.length > 0)
       saveNullifiers(transactionNullifiers, transaction.blockNumber); // we can now safely store the nullifiers IFF they are present
-    saveTransaction({ ...transaction, mempool }); // then we need to save it
+    if (mempool) transaction.mempool = mempool;
+    saveTransaction({ ...transaction }); // then we need to save it
   } catch (err) {
     if (err instanceof TransactionError)
       logger.warn(
