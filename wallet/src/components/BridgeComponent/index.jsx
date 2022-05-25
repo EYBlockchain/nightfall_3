e70270564fb29899cb69cdffbca105ddcf144330
Luiz <luiz.oamorim@gmail.com>
2022-05-25 15:32:22 -0300

    fix: removing hardcoded amount restriction for deposit

--- wallet/src/components/BridgeComponent/index.jsx
+++ wallet/src/components/BridgeComponent/index.jsx
@@ -328,9 +328,9 @@ const BridgeComponent = () => {
     if (
       txType === 'deposit' &&
       new BigFloat(transferValue, token.decimals).toBigInt() >
-        new BigFloat('0.25', token.decimals).toBigInt()
-    ) {
-      toast.error("Input value can't be greater than 0.25");
+        parseInt(token.restrictions[txType])
+    ) {      
+      toast.error(`Input value can't be greater than ${parseInt(token.restrictions[txType])/parseInt(new BigFloat('1', token.decimals).toBigInt().toString())}`);
       return;
     }
 
