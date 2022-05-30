eacfa995b260d64853b3a1377603790da035d9f3
daveroga <daveroga@gmail.com>
2022-05-30 19:26:06 +0200

    fix: unused functions

--- nightfall-challenger/src/nf3-wrapper.mjs
+++ nightfall-challenger/src/nf3-wrapper.mjs
@@ -2,20 +2,6 @@ import Nf3 from '../../cli/lib/nf3.mjs';
 
 let nf3Instance = '';
 
-async function nf3SendOffchainTransaction(transaction) {
-  if (nf3Instance !== '') {
-    await nf3Instance.sendOffchainTransaction(transaction);
-  }
-}
-
-async function nf3GetContractAddressOptimist(contract) {
-  if (nf3Instance !== '') {
-    const address = await nf3Instance.getContractAddressOptimist(contract);
-    return address;
-  }
-  return null;
-}
-
 async function nf3Init(challengerEthereumSigningKey, Urls, mnemonic, contractAddressProvider) {
   nf3Instance = new Nf3(challengerEthereumSigningKey, Urls);
   if (nf3Instance !== '') {
@@ -47,11 +33,4 @@ async function nf3StartChallenger() {
   return false;
 }
 
-export {
-  nf3SendOffchainTransaction,
-  nf3GetContractAddressOptimist,
-  nf3Init,
-  nf3Healthcheck,
-  nf3RegisterChallenger,
-  nf3StartChallenger,
-};
+export { nf3Init, nf3Healthcheck, nf3RegisterChallenger, nf3StartChallenger };
