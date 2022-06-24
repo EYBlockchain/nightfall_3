32dce777b4eeff59b41bd630da97971f91b25c09
David Rodriguez Garcia <70919589+daveroga@users.noreply.github.com>
2022-06-25 01:09:48 +0200

    Test with miner.gastarget 15M

--- config/geth/entrypoint.sh
+++ config/geth/entrypoint.sh
@@ -17,7 +17,7 @@ exec geth --bootnodes=`cat /setup/bootnodes` \
   --datadir=/data \
   --allow-insecure-unlock \
   --miner.etherbase=0x00a329c0648769a73afac7f9381e08fb43dbea72 \
-  --miner.gastarget 30000000 \
+  --miner.gastarget 15000000 \
   --miner.gaslimit 30000000 \
   --miner.threads=1 \
   "$@"
