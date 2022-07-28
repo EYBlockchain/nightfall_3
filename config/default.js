f915c416de669e73caf27fb7dfdfb3eba1c867ec
Eugenio <eugypalu@gmail.com>
2022-07-28 18:13:05 +0200

    feat: added lambda url and mq credential

--- config/default.js
+++ config/default.js
@@ -427,4 +427,8 @@ module.exports = {
   // AMAZONMQ
   usernameMq: process.env.USERNAME_MQ,
   pswMQ: process.env.PASSWORD_MQ,
+
+  // LAMBDA TWOSTEPSYNC
+  twoStepSyncUrl: process.env.TWO_STEP_SYNC_URL,
+  twoStepSyncDeployment: process.env.TWO_STEP_SYNC_DEPLOYMENT,
 };
