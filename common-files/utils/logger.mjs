0041d1508357d4fecc5278fe4c0fa4aef6a7b69d
David <druiz0992@gmail.com>
2022-08-04 21:32:01 +0200

    fix: eslint

--- common-files/utils/logger.mjs
+++ common-files/utils/logger.mjs
@@ -3,15 +3,15 @@
 import config from 'config';
 import pino from 'pino';
 
-const LOGGER_TIME_STRING = "yyyy-mm-dd HH:MM:ss";
+const LOGGER_TIME_STRING = 'yyyy-mm-dd HH:MM:ss';
 
 export default pino({
   level: config.LOG_LEVEL,
   transport: {
-    target: "pino-pretty",
+    target: 'pino-pretty',
     options: {
       colorize: true,
-      ignore: "pid,hostname,filename",
+      ignore: 'pid,hostname,filename',
       translateTime: LOGGER_TIME_STRING,
     },
   },
