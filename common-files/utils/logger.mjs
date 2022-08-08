3772db0a864526dec33679606a4f49c5b7fe8b5e
Israel Boudoux <iboudoux@polygon.technology>
2022-08-08 02:39:01 +0100

    fix: adding millis to the date format

--- common-files/utils/logger.mjs
+++ common-files/utils/logger.mjs
@@ -3,11 +3,12 @@
 import config from 'config';
 import pino from 'pino';
 
-const LOGGER_TIME_STRING = 'yyyy-mm-dd HH:MM:ss';
+const LOGGER_TIME_STRING = 'yyyy-mm-dd HH:MM:ss.l';
 
 export default pino({
   level: config.LOG_LEVEL || 'info',
   formatters: {
+    // eslint-disable-next-line no-unused-vars
     level (label, number) {
       return { level: label };
     }
