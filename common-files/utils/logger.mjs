2740436fa3d8248bd303d7f7093041097ec9c6c3
Israel Boudoux <iboudoux@polygon.technology>
2022-08-08 23:04:30 +0100

    fix: adding pino-pretty only for dev environments

--- common-files/utils/logger.mjs
+++ common-files/utils/logger.mjs
@@ -1,24 +1,41 @@
 /* eslint import/no-extraneous-dependencies: "off" */
+/* eslint no-unused-vars: "off" */
 
 import config from 'config';
 import pino from 'pino';
+import isDev from './utils.mjs';
 
 const LOGGER_TIME_STRING = 'yyyy-mm-dd HH:MM:ss.l';
 
-export default pino({
-  level: config.LOG_LEVEL || 'info',
-  formatters: {
-    // eslint-disable-next-line no-unused-vars
-    level (label, number) {
-      return { level: label };
-    }
-  },
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      ignore: 'pid,hostname,filename',
-      translateTime: LOGGER_TIME_STRING,
+const getInstance = (isDev) => {
+  const pinoOptions = {
+    level: config.LOG_LEVEL || 'info',
+    formatters: {
+      level (label, number) { // echoes the level as the label instead of the number
+        return { level: label };
+      },
+      bindings (bindings) { // removes the pid and hostname fields from the logs
+        return {};
+      }
     },
-  },
-});
+    timestamp: () => `,"time": "${new Date(Date.now()).toISOString()}"`,
+    transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          ignore: 'pid,hostname,filename',
+          translateTime: LOGGER_TIME_STRING,
+        },
+    }
+  };
+
+  if(! isDev) {
+    delete pinoOptions.transport;
+  }
+
+  return pino(pinoOptions);
+}
+
+const instance = getInstance(isDev());
+
+export default instance;
