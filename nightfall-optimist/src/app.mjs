6d7b19bc3aed6acae861cb87cfb61676d382595f
Israel Boudoux <iboudoux@polygon.technology>
2022-08-09 14:32:47 +0100

    fix: eslint

--- nightfall-optimist/src/app.mjs
+++ nightfall-optimist/src/app.mjs
@@ -1,4 +1,7 @@
+/* eslint no-shadow: "off" */
+
 import express from 'express';
+import { setupHttpDefaults } from 'common-files/utils/httputils.mjs';
 import {
   proposer,
   block,
@@ -7,17 +10,21 @@ import {
   getContractAddress,
   debug,
 } from './routes/index.mjs';
-import { setupHttpDefaults } from 'common-files/utils/httputils.mjs';
 
 const app = express();
 
-setupHttpDefaults(app, app => {
-  app.use('/proposer', proposer);
-  app.use('/block', block);
-  app.use('/challenger', challenger);
-  app.use('/transaction', transaction);
-  app.use('/contract-address', getContractAddress);
-  app.use('/debug', debug);
-}, true, true);
+setupHttpDefaults(
+  app,
+  app => {
+    app.use('/proposer', proposer);
+    app.use('/block', block);
+    app.use('/challenger', challenger);
+    app.use('/transaction', transaction);
+    app.use('/contract-address', getContractAddress);
+    app.use('/debug', debug);
+  },
+  true,
+  true,
+);
 
 export default app;
--- zokrates-worker/src/app.mjs
+++ zokrates-worker/src/app.mjs
@@ -1,19 +1,26 @@
+/* eslint no-shadow: "off" */
+
 import express from 'express';
+import { setupHttpDefaults } from 'common-files/utils/httputils.mjs';
 import generateProof from './routes/generateProof.mjs';
 import generateKeys from './routes/generateKeys.mjs';
 import vk from './routes/vk.mjs';
 import loadCircuits from './routes/loadCircuits.mjs';
 import verify from './routes/verify.mjs';
-import { setupHttpDefaults } from 'common-files/utils/httputils.mjs';
 
 const app = express();
 
-setupHttpDefaults(app, app => {
-  app.use('/generate-keys', generateKeys);
-  app.use('/generate-proof', generateProof);
-  app.use('/vk', vk);
-  app.use('/load-circuits', loadCircuits);
-  app.use('/verify', verify);
-}, true, true);
+setupHttpDefaults(
+  app,
+  app => {
+    app.use('/generate-keys', generateKeys);
+    app.use('/generate-proof', generateProof);
+    app.use('/vk', vk);
+    app.use('/load-circuits', loadCircuits);
+    app.use('/verify', verify);
+  },
+  true,
+  true,
+);
 
 export default app;
