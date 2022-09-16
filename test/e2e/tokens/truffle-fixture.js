73522be5c236afcdce20c71f83d5dca333ff48c5
siosw <github@simonoswald.xyz>
2022-09-16 11:02:30 +0200

    chore: remove unnessecary fixture

--- test/e2e/tokens/truffle-fixture.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require('Migrations');
-
-module.exports = async () => {
-  //const migrations = await Migrations.new();
-  //Migrations.setAsDeployed(migrations);
-  //console.log('deployed migrations!');
-};
