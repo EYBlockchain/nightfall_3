6491649bb18805c0e83fd9ca8c94d3ed7984f3ea
Jose Pedro Sousa <github@zepedro.me>
2022-09-22 15:15:33 +0100

    chore: adding comment explaining the use of encodeABI() over send()

--- nightfall-deployer/src/circuit-setup.mjs
+++ nightfall-deployer/src/circuit-setup.mjs
@@ -132,6 +132,8 @@ async function setupCircuits() {
       );
 
       // when using a private key, we shouldn't assume an unlocked account and we sign the transaction directly
+      // on networks like Edge, there's no account management so we need to encodeABI()
+      // since methods like send() don't work
       if (config.ETH_PRIVATE_KEY) {
         const tx = {
           from: process.env.FROM_ADDRESS,
