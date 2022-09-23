f0927478958645cf9b743673aa73f5e7bbeb1f14
Eugenio Paluello <eugypalu@gmail.com>
2022-09-23 12:23:55 +0200

    feat: mq link for each env

---
+++ wallet/src/common-files/utils/mq.js
@@ -0,0 +1,11 @@
+/* ignore unused exports */
+export const wsMqMapping = {
+    preprod: 'wss://b-9e0fa456-f936-4f26-baee-55dd3582ae03-1.mq.eu-west-1.amazonaws.com:61619',
+    testnet: 'wss://b-9395247a-394f-4216-a06d-b5f2478ebaae-1.mq.eu-west-1.amazonaws.com:61619',
+    production: 'wss://b-bdc17047-421f-46de-a2dc-e00d2b8aa5db-1.mq.eu-west-1.amazonaws.com:61619',
+    staging: 'wss://b-def6fb96-740f-49b5-b640-1bb7e6da0fac-1.mq.eu-west-1.amazonaws.com:61619',
+    internal: 'wss://b-1a9dd658-d72d-4209-81fb-faee9e4384e7-1.mq.eu-west-1.amazonaws.com:61619',
+};
+
+export const topicRollback = 'rollback';
+export const topicBlockProposed = 'blockProposed';
