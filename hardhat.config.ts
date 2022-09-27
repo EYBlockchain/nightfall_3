d66e37b1e300597c108901517f0fd010abb81836
siosw <git@simonoswald.xyz>
2022-09-27 10:59:16 +0200

    chore: upgrade to hardhat@esm + remove custom task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,14 +2,15 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-truffle5';
 import '@nomicfoundation/hardhat-toolbox';
 
-import './hardhat.test-esm-task';
-
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   paths: {
     sources: './nightfall-deployer/contracts',
     tests: './test/e2e',
   },
+  mocha: {
+    timeout: 0
+  }
 };
 
 export default config;
