bb308ce1828ac3d49db812c6268322358c85574e
RogerTaule <roger.taulee@gmail.com>
2022-09-28 13:41:34 +0200

    feat: user can select which commitments to use

--- nightfall-client/src/utils/getCommitmentInfo.mjs
+++ nightfall-client/src/utils/getCommitmentInfo.mjs
@@ -5,8 +5,10 @@ import constants from '@polygon-nightfall/common-files/constants/index.mjs';
 import Nullifier from '../classes/nullifier.mjs';
 import {
   clearPending,
+  markPending,
   findUsableCommitmentsMutex,
   getSiblingInfo,
+  getCommitmentsByHash,
 } from '../services/commitment-storage.mjs';
 import Commitment from '../classes/commitment.mjs';
 import { ZkpKeys } from '../services/keys.mjs';
@@ -25,9 +27,10 @@ export const getCommitmentInfo = async txInfo => {
     maticAddress,
     tokenId = generalise(0),
     rootKey,
-    maxNumberNullifiers,
-    onlyFee = false,
+    providedCommitments,
   } = txInfo;
+
+  let { maxNumberNullifiers, onlyFee = false } = txInfo;
 
   const { zkpPublicKey, compressedZkpPublicKey, nullifierKey } = new ZkpKeys(rootKey);
 
@@ -41,39 +44,94 @@ export const getCommitmentInfo = async txInfo => {
 
   logger.debug(`Fee will be added as part of the transaction commitments: ${addedFee > 0n}`);
 
-  const value = totalValueToSend + addedFee;
+  let value = totalValueToSend + addedFee;
   const feeValue = fee.bigInt - addedFee;
 
-  const commitments = await findUsableCommitmentsMutex(
-    compressedZkpPublicKey,
-    ercAddress,
-    tokenId,
-    maticAddress,
-    value,
-    feeValue,
-    maxNumberNullifiers,
-    onlyFee,
-  );
+  logger.debug(`using user provided commitments: ${providedCommitments !== undefined}`);
 
-  if (!commitments) throw new Error('Not available commitments has been found');
-
-  const { oldCommitments, oldCommitmentsFee } = commitments;
-
-  logger.debug({
-    msg: `Found commitments ${addedFee > 0n ? 'including fee' : ''}`,
-    oldCommitments,
-  });
-
-  if (feeValue > 0n) {
-    logger.debug({
-      msg: 'Found commitments fee',
-      oldCommitmentsFee,
-    });
-  }
-
-  const spentCommitments = [...oldCommitments, ...oldCommitmentsFee];
-
+  const spentCommitments = [];
   try {
+    let validatedProvidedCommitments = [];
+    if (providedCommitments) {
+      const commitmentHashes = providedCommitments.map(c => c.toString());
+      logger.debug({ msg: 'looking up these commitment hashes:', commitmentHashes });
+      const rawCommitments = await getCommitmentsByHash(
+        commitmentHashes,
+        compressedZkpPublicKey,
+        ercAddress,
+        tokenId,
+      );
+
+      if (rawCommitments.length < commitmentHashes.length) {
+        const invalidHashes = commitmentHashes.filter(ch => {
+          for (const rc of rawCommitments) {
+            if (rc._id === ch) return false;
+          }
+          return true;
+        });
+        throw new Error(`invalid commitment hashes: ${invalidHashes}`);
+      }
+
+      const providedValue = rawCommitments
+        .map(c => generalise(c.preimage.value).bigInt)
+        .reduce((sum, c) => sum + c);
+
+      if (providedValue < totalValueToSend)
+        throw new Error('provided commitments do not cover the value');
+
+      // transform the hashes retrieved from the DB to well formed commitments
+      validatedProvidedCommitments = rawCommitments.map(ct => new Commitment(ct.preimage));
+      logger.debug({ providedValue });
+
+      if (maticAddress.hex(32) === ercAddress.hex(32)) {
+        // the user provieded enough commitments to cover the value but not the fee
+        // this can only happen when the token to send is the fee token
+        // we need to set the value here instead of the feeValue
+        value = providedValue >= value ? 0n : value - providedValue;
+        onlyFee = value === 0n;
+      } else {
+        onlyFee = true;
+      }
+
+      maxNumberNullifiers -= validatedProvidedCommitments.length;
+
+      await Promise.all(validatedProvidedCommitments.map(c => markPending(c)));
+      spentCommitments.push(...validatedProvidedCommitments);
+    }
+
+    logger.debug({
+      msg: 'find usable commitments inputs',
+      value,
+      feeValue,
+      onlyFee,
+    });
+    const commitments = await findUsableCommitmentsMutex(
+      compressedZkpPublicKey,
+      ercAddress,
+      tokenId,
+      maticAddress,
+      value,
+      feeValue,
+      maxNumberNullifiers,
+      onlyFee,
+    );
+    logger.debug({ commitments });
+    const { oldCommitments, oldCommitmentsFee } = commitments;
+
+    spentCommitments.push(...oldCommitments);
+    spentCommitments.push(...oldCommitmentsFee);
+    oldCommitments.push(...validatedProvidedCommitments);
+
+    logger.debug(
+      `Found commitments ${addedFee > 0n ? 'including fee' : ''} ${oldCommitments.map(c =>
+        JSON.stringify({ addr: c.preimage.ercAddress.hex(32), value: c.preimage.value.bigInt }),
+      )}`,
+    );
+
+    if (feeValue > 0n) {
+      logger.debug(`Found commitments fee ${JSON.stringify(oldCommitmentsFee, null, 2)}`);
+    }
+
     // Compute the nullifiers
     const nullifiers = spentCommitments.map(commitment => new Nullifier(commitment, nullifierKey));
 
@@ -84,7 +142,9 @@ export const getCommitmentInfo = async txInfo => {
     );
 
     // we may need to return change to the recipient
-    const change = totalInputCommitmentValue - value;
+    const change = totalInputCommitmentValue - (totalValueToSend + addedFee);
+
+    logger.debug({ totalInputCommitmentValue, change });
 
     // if so, add an output commitment to do that
     if (change !== 0n) {
@@ -102,6 +162,8 @@ export const getCommitmentInfo = async txInfo => {
 
     // we may need to return change to the recipient
     const changeFee = totalInputCommitmentFeeValue - feeValue;
+
+    logger.debug({ totalInputCommitmentFeeValue, changeFee });
 
     // if so, add an output commitment to do that
     if (changeFee !== 0n) {
