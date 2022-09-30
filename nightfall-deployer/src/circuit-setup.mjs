ba3bb4fc4620ad040523491440e0aa81321552dc
Eugenio Paluello <eugypalu@gmail.com>
2022-09-30 09:28:47 +0200

    fix: check if vk is null and force trusted setup

--- nightfall-deployer/src/circuit-setup.mjs
+++ nightfall-deployer/src/circuit-setup.mjs
@@ -104,7 +104,7 @@ async function setupCircuits() {
       },
     );
 
-    if (checkHash.data || config.ALWAYS_DO_TRUSTED_SETUP) {
+    if (checkHash.data || !vks[i] || config.ALWAYS_DO_TRUSTED_SETUP) {
       try {
         logger.info({
           msg: 'No existing verification key. Fear not, I will make a new one: calling generate keys',
