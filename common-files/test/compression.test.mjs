1c6364344b1c5d9b60db157bd5fc4192660dee85
RogerTaule <roger.taulee@gmail.com>
2022-10-02 16:22:56 +0200

    fix: compression test

--- common-files/test/compression.test.mjs
+++ common-files/test/compression.test.mjs
@@ -9,11 +9,11 @@ import {
 } from 'common-files/utils/curve-maths/curves.mjs';
 
 const testProof = {
-  a: [
+  pi_a: [
     '0x2d79b57403a0bc4a65c4953b0b63052de3264282e045b25b00af4ca310c0acde',
     '0x0adc4514da97d5b629f08a0021273d38e84948c5e8dd9d26518ef8f62b25e404',
   ],
-  b: [
+  pi_b: [
     [
       '0x0a875f4b4debdc23b6420be63c11980dcd662e7a7d40dfe3906f14b258fb547b',
       '0x10ffa8e73f358543dd617aa0a7a29f3c4d11a4664d8f6e4bea26c2a02345304f',
@@ -23,7 +23,7 @@ const testProof = {
       '0x16bfa75d11f7e47c71ae733bbaa784e6166bf3ce6d20b5510f1cff17b13899a9',
     ],
   ],
-  c: [
+  pi_c: [
     '0x270fc6470007376b20ebb4bb5bd44effa470df4db6ce8284e9eeda02f113d105',
     '0x2914ac1373b129494fa2559199259685bbbd66d3bc77e3303d8775ddf5df74b5',
   ],
