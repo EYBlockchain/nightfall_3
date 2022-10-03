85dd039a0e5668c6db36971926445b971fc08519
Westlad <Duncan.Westland@uk.ey.com>
2022-10-03 11:26:59 +0100

    fix: pr comments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import 'hardhat-contract-sizer';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.2',
+    version: '0.8.3',
     settings: {
       optimizer: {
         enabled: true,
--- nightfall-client/src/routes/kyc.mjs
+++ nightfall-client/src/routes/kyc.mjs
@@ -9,14 +9,12 @@ import { addUserToWhitelist, removeUserFromWhitelist, isWhitelisted } from '../s
 const router = express.Router();
 
 router.get('/check', async (req, res, next) => {
-  logger.debug('kyc/check endpoint received GET');
   try {
     const { address } = req.query;
     logger.debug(`Details requested with address ${address}`);
     const whitelisted = await isWhitelisted(address);
     res.json({ isWhitelisted: whitelisted });
   } catch (err) {
-    logger.error(err);
     next(err);
   }
 });
@@ -25,13 +23,11 @@ router.get('/check', async (req, res, next) => {
  Add a use to a KYC whitelist (only works if user is a whitelist manager, otherwise just wastes gas)
  */
 router.post('/add', async (req, res, next) => {
-  logger.debug('kyc/add endpoint received POST');
   const { address } = req.body;
   try {
     const response = await addUserToWhitelist(address);
     res.json(response);
   } catch (err) {
-    logger.error(err);
     next(err);
   }
 });
@@ -40,13 +36,11 @@ router.post('/add', async (req, res, next) => {
  Add a use to a KYC whitelist (only works if user is a relevant (to the group) whitelist manager, otherwise just wastes gas)
  */
 router.post('/remove', async (req, res, next) => {
-  logger.debug('kyc/remove endpoint received POST');
   const { address } = req.body;
   try {
     const response = await removeUserFromWhitelist(address);
     res.json(response);
   } catch (err) {
-    logger.error(err);
     next(err);
   }
 });
