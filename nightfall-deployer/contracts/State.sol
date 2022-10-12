96839b281901cf6c630349f772362e5c7a2a242c
Roger Taulé Buxadera <55488871+RogerTaule@users.noreply.github.com>
2022-10-12 12:13:01 +0200

    Fees upgrade (#1043)
    * refactor: moving padArray to common-files
    
    * fix: fixing wallet imports
    
    * fix: modifying fee meaning in the transaction struct
    
    * fix: rebase mistake

--- nightfall-deployer/contracts/State.sol
+++ nightfall-deployer/contracts/State.sol
@@ -23,7 +23,7 @@ contract State is ReentrancyGuardUpgradeable, Pausable, Config {
     mapping(address => FeeTokens) public pendingWithdrawalsFees;
     mapping(address => LinkedAddress) public proposers;
     mapping(address => TimeLockedStake) public stakeAccounts;
-    mapping(bytes32 => FeeTokens) public feeBook;
+    mapping(bytes32 => FeeTokens) public feeBookBlocks;
     mapping(bytes32 => bool) public claimedBlockStakes;
     LinkedAddress public currentProposer; // who can propose a new shield state
     uint256 public proposerStartBlock; // L1 block where currentProposer became current
@@ -122,22 +122,14 @@ contract State is ReentrancyGuardUpgradeable, Pausable, Config {
         stake.challengeLocked += blockStake;
         stakeAccounts[msg.sender] = TimeLockedStake(stake.amount, stake.challengeLocked, 0);
 
-        bytes32 input = keccak256(abi.encodePacked(b.proposer, b.blockNumberL2));
-        feeBook[input].feesEth = 0;
-        feeBook[input].feesMatic = 0;
-        for (uint256 i = 0; i < t.length; i++) {
-            if (t[i].transactionType == TransactionTypes.DEPOSIT) {
-                feeBook[input].feesEth += uint256(t[i].fee);
-            } else {
-                feeBook[input].feesMatic += uint256(t[i].fee);
-            }
-        }
+        FeeTokens memory feePayments = FeeTokens(0, 0);
 
         bytes32 blockHash;
         uint256 blockSlots = BLOCK_STRUCTURE_SLOTS; //Number of slots that the block structure has
 
         //Get the signature for the function that checks if the transaction has been escrowed or not
         bytes4 checkTxEscrowedSignature = bytes4(keccak256('getTransactionEscrowed(bytes32)'));
+        bytes4 checkTxEthFeesSignature = bytes4(keccak256('getTransactionEthFee(bytes32)'));
 
         assembly {
             //Function that calculates the height of the Merkle Tree
@@ -225,6 +217,54 @@ contract State is ReentrancyGuardUpgradeable, Pausable, Config {
                         revert(0, 0)
                     }
                 }
+
+                if iszero(
+                    calldataload(
+                        add(add(t.offset, calldataload(add(t.offset, mul(0x20, i)))), mul(0x20, 1))
+                    )
+                ) {
+                    mstore(x, checkTxEthFeesSignature) //Store the signature of the function in x
+                    mstore(add(x, 0x04), mload(add(transactionHashesPos, mul(0x20, i)))) //Store the transactionHash after the signature
+                    pop(
+                        call(
+                            // Call getTransactionFeesEth function to see if funds has been deposited
+                            gas(),
+                            shieldAddress.slot, //To addr
+                            0, //No value
+                            x, //Inputs are stored at location x
+                            add(0x04, 0x20), //Inputs are 36 bytes long
+                            x, //Store output over input (saves space)
+                            0x20 //Outputs are 32 bytes long
+                        )
+                    )
+
+                    mstore(feePayments, add(mload(x), mload(feePayments)))
+                }
+
+                if eq(
+                    iszero(
+                        calldataload(
+                            add(
+                                add(t.offset, calldataload(add(t.offset, mul(0x20, i)))),
+                                mul(0x20, 1)
+                            )
+                        )
+                    ),
+                    0
+                ) {
+                    mstore(
+                        add(feePayments, 0x20),
+                        add(
+                            calldataload(
+                                add(
+                                    add(t.offset, calldataload(add(t.offset, mul(0x20, i)))),
+                                    mul(0x20, 1)
+                                )
+                            ),
+                            mload(add(feePayments, 0x20))
+                        )
+                    )
+                }
             }
 
             //Calculate the root of the transactions merkle tree
@@ -272,6 +312,9 @@ contract State is ReentrancyGuardUpgradeable, Pausable, Config {
         // contain all of the relevant data (2) it doesn't take much gas.
         // All check pass so add the block to the list of blocks waiting to be permanently added to the state - we only save the hash of the block data plus the absolute minimum of metadata - it's up to the challenger, or person requesting inclusion of the block to the permanent contract state, to provide the block data.
 
+        // Store block fees
+        feeBookBlocks[keccak256(abi.encodePacked(b.proposer, b.blockNumberL2))] = feePayments;
+
         // blockHash is hash of all block data and hash of all the transactions data.
         blockHashes.push(
             BlockData({
@@ -311,17 +354,17 @@ contract State is ReentrancyGuardUpgradeable, Pausable, Config {
         return currentProposer;
     }
 
-    function getFeeBookInfo(address proposer, uint256 blockNumberL2)
+    function getFeeBookBlocksInfo(address proposer, uint256 blockNumberL2)
         public
         view
         returns (FeeTokens memory)
     {
         bytes32 input = keccak256(abi.encodePacked(proposer, blockNumberL2));
-        return (feeBook[input]);
+        return (feeBookBlocks[input]);
     }
 
-    function resetFeeBookInfo(address proposer, uint256 blockNumberL2) public onlyShield {
-        delete feeBook[keccak256(abi.encodePacked(proposer, blockNumberL2))];
+    function resetFeeBookBlocksInfo(address proposer, uint256 blockNumberL2) public onlyShield {
+        delete feeBookBlocks[keccak256(abi.encodePacked(proposer, blockNumberL2))];
     }
 
     function popBlockData() public onlyChallenger returns (BlockData memory) {
@@ -437,7 +480,7 @@ contract State is ReentrancyGuardUpgradeable, Pausable, Config {
     // Checks if a block is actually referenced in the queue of blocks waiting
     // to go into the Shield state (stops someone challenging with a non-existent
     // block).
-    function isBlockReal(Block calldata b) public view {
+    function isBlockReal(Block calldata b) public view returns (bytes32) {
         bytes32 blockHash = Utils.hashBlock(b);
         require(
             b.blockNumberL2 < blockHashes.length &&
