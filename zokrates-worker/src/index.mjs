4cc82dbe3794fd9a098638810cfab92c1732c9b4
David <druiz0992@gmail.com>
2022-10-13 14:49:15 +0200

    fix: add GHA

--- zokrates-worker/src/index.mjs
+++ zokrates-worker/src/index.mjs
@@ -44,7 +44,8 @@ const checkCircuitsOutput = async () => {
       files.map(async f => {
         if (f) {
           const filename = f.split('  ')[1];
-          const circuit = circuits.find(c => filename.includes(c));
+          const _circuit = circuits.find(c => filename.includes(c));
+          const circuit = typeof _circuit === 'undefined' ? '.' : _circuit;
 
           if (!fs.existsSync(`${outputPath}/${circuit}`)) {
             fs.mkdirSync(`${outputPath}/${circuit}`);
