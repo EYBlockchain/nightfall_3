1a8fe9fcb3c9b67a09abfe464acb627cb02679d5
Eugenio Paluello <eugypalu@gmail.com>
2022-10-31 16:30:15 +0100

    fix: eslint

--- test/unit/SmartContracts/state.unit.test.mjs
+++ test/unit/SmartContracts/state.unit.test.mjs
@@ -5,7 +5,6 @@ import { setTransactionInfo } from '../utils/shieldStorage.mjs';
 
 const { ethers, upgrades } = hardhat;
 
-
 describe('State contract State functions', function () {
   let ProposersInstance;
   let addr1;
