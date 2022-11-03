76491d60844f311ffb754580008835b2feaef6d9
Liju Jose <liju.jose@gds.ey.com>
2022-11-03 16:07:22 +0530

    fix: make adversary proposer to submit bad transaction

--- cli/lib/nf3.mjs
+++ cli/lib/nf3.mjs
@@ -1018,6 +1018,36 @@ class Nf3 {
 
       if (type === 'rollback') proposeEmitter.emit('rollback', data);
 
+      // this is used by adversary proposer for submitting bad transaction.
+      if (type === 'submit-transaction') {
+        try {
+          const ercAddress = await this.getContractAddressOptimist('ERC20Mock');
+          const approvetxDataToSign = await approve(
+            ercAddress,
+            this.ethereumAddress,
+            this.shieldContractAddress,
+            (transactions[0].tokenType === '0' && 'ERC20') ||
+              (transactions[0].tokenType === '1' && 'ERC721') ||
+              (transactions[0].tokenType === '2' && 'ERC1155'),
+            transactions[0].value,
+            this.web3,
+            !!this.ethereumSigningKey,
+          );
+          if (approvetxDataToSign) await this.submitTransaction(approvetxDataToSign, ercAddress, 0);
+          const receipt = await this.submitTransaction(
+            txDataToSign,
+            this.shieldContractAddress,
+            Number(transactions[0].fee),
+          );
+          proposeEmitter.emit('submit-transaction-receipt', receipt, transactions);
+        } catch (err) {
+          logger.error({
+            msg: 'Error while trying to submit a submit-transaction',
+            err,
+          });
+        }
+      }
+
       return null;
     };
 
