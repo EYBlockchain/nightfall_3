437acf929887231a59d0c86edda58efe948959a0
Westlad <Duncan.Westland@uk.ey.com>
2022-11-07 10:19:31 +0000

    fix: point client whitelist functions at KYC(X509) contract

--- nightfall-client/src/services/kyc.mjs
+++ nightfall-client/src/services/kyc.mjs
@@ -5,19 +5,19 @@
 import constants from '@polygon-nightfall/common-files/constants/index.mjs';
 import { waitForContract } from '@polygon-nightfall/common-files/utils/contract.mjs';
 
-const { SHIELD_CONTRACT_NAME } = constants;
+const { KYC_CONTRACT_NAME } = constants;
 
 export async function isWhitelisted(address) {
-  const shieldContractInstance = await waitForContract(SHIELD_CONTRACT_NAME);
+  const shieldContractInstance = await waitForContract(KYC_CONTRACT_NAME);
   return shieldContractInstance.methods.isWhitelisted(address).call();
 }
 
 export async function addUserToWhitelist(address) {
-  const shieldContractInstance = await waitForContract(SHIELD_CONTRACT_NAME);
+  const shieldContractInstance = await waitForContract(KYC_CONTRACT_NAME);
   return shieldContractInstance.methods.addUserToWhitelist(address).encodeABI();
 }
 
 export async function removeUserFromWhitelist(address) {
-  const shieldContractInstance = await waitForContract(SHIELD_CONTRACT_NAME);
+  const shieldContractInstance = await waitForContract(KYC_CONTRACT_NAME);
   return shieldContractInstance.methods.removeUserFromWhitelist(address).encodeABI();
 }
