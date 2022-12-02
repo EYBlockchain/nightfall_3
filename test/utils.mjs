6adfb7692b117a6f847f7062dc13d6458b8bf654
VelkoskiS <svelkoskis@polygon.technology>
2022-12-02 08:58:45 +0100

    fix: eslint

--- test/utils.mjs
+++ test/utils.mjs
@@ -64,7 +64,7 @@ export class Web3Client {
   }
 
   async getTransactionReceipt(txHash) {
-    return await this.web3.eth.getTransactionReceipt(txHash);
+    return this.web3.eth.getTransactionReceipt(txHash);
   }
 
   subscribeTo(event, queue, options) {
