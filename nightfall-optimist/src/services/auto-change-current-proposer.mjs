cedba58ff8b6427ce8f82239deccc2b0c4bb31b4
Eugenio Paluello <eugypalu@gmail.com>
2022-12-18 02:19:11 +0100

    fix: eslint

--- nightfall-optimist/src/services/auto-change-current-proposer.mjs
+++ nightfall-optimist/src/services/auto-change-current-proposer.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import { waitForContract } from '@polygon-nightfall/common-files/utils/contract.mjs';
 import constants from '@polygon-nightfall/common-files/constants/index.mjs';
 import Web3 from '@polygon-nightfall/common-files/utils/web3.mjs';
@@ -12,10 +13,13 @@ async function autoChangeCurrentProposer(proposer) {
   const web3 = Web3.connection();
   const stateContractInstance = await waitForContract(STATE_CONTRACT_NAME);
 
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     logger.info('Checking Proposer...');
     const proposerStartBlock = await stateContractInstance.methods.proposerStartBlock().call();
-    const rotateProposerBlocks = await stateContractInstance.methods.getRotateProposerBlocks().call();
+    const rotateProposerBlocks = await stateContractInstance.methods
+      .getRotateProposerBlocks()
+      .call();
     const numproposers = await stateContractInstance.methods.getNumProposers().call();
     const currentSprint = await stateContractInstance.methods.currentSprint().call();
     const currentBlock = await web3.eth.getBlockNumber();
@@ -25,12 +29,14 @@ async function autoChangeCurrentProposer(proposer) {
       if (currentSprint === '0') {
         let spanProposersList = [];
         for (let i = 0; i < sprintInSpan; i++) {
-          spanProposersList.push(stateContractInstance.methods.spanProposersList(i).call())
+          spanProposersList.push(stateContractInstance.methods.spanProposersList(i).call());
         }
         spanProposersList = await Promise.all(spanProposersList);
         logger.info(`list of next proposer: ${spanProposersList}`);
       }
-      const spanProposersListAtPosition = await stateContractInstance.methods.spanProposersList(currentSprint).call();
+      const spanProposersListAtPosition = await stateContractInstance.methods
+        .spanProposersList(currentSprint)
+        .call();
       logger.info(`Proposer address: ${spanProposersListAtPosition} and sprint: ${currentSprint}`);
       try {
         if (spanProposersListAtPosition === proposer.address) {
