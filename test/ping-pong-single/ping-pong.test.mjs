e024dd7686f0f13efe015193544c6336aef11249
Israel Boudoux <iboudoux@polygon.technology>
2023-01-04 16:01:09 +0000

    fix: adding pp single

---
+++ test/ping-pong-single/ping-pong.test.mjs
@@ -0,0 +1,31 @@
+import config from 'config';
+import localTest from './index.mjs';
+import Nf3 from '../../cli/lib/nf3.mjs';
+
+const environment = config.ENVIRONMENTS[process.env.ENVIRONMENT] || config.ENVIRONMENTS.localhost;
+
+const { mnemonics, signingKeys } = config.TEST_OPTIONS;
+const nf3Proposer = new Nf3(signingKeys.proposer1, environment);
+
+describe('Ping-pong tests', () => {
+  before(async () => {
+    if (process.env.ENVIRONMENT !== 'aws') {
+      await nf3Proposer.init(mnemonics.proposer);
+      // we must set the URL from the point of view of the client container
+      await nf3Proposer.registerProposer('http://optimist', await nf3Proposer.getMinimumStake());
+      await nf3Proposer.startProposer();
+    }
+  });
+
+  it('Runs ping-pong tests', async () => {
+    localTest(true);
+    await localTest(false);
+  });
+
+  after(async () => {
+    if (process.env.ENVIRONMENT !== 'aws') {
+      await nf3Proposer.deregisterProposer();
+      await nf3Proposer.close();
+    }
+  });
+});
