ec70473868336a16d23af41f5f6f675056296ab1
imagobea <himagobea@gmail.com>
2023-01-18 13:20:06 +0100

    chore: rm commented code in optimist debug counters

--- nightfall-optimist/src/services/debug-counters.mjs
+++ nightfall-optimist/src/services/debug-counters.mjs
@@ -1,12 +1,6 @@
 const debugCounters = {
   // CheckBlock returns a failure
   nBlockInvalid: 0,
-  // Proposer WS is closed
-  // proposerWsClosed: 0,
-  // Proposer WS is closed and couldn't be opened
-  // proposerWsFailed: 0,
-  // Optimist couldn't proposed blocks because of WS
-  // proposerBlockNotSent: 0,
 };
 
 /**
