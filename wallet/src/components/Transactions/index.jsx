637e17e66fa5a551626f6932f66c5667786d93a5
David <druiz0992@gmail.com>
2023-01-19 10:19:49 +0100

    fix: eslint

--- wallet/src/components/Transactions/index.jsx
+++ wallet/src/components/Transactions/index.jsx
@@ -43,12 +43,6 @@ const Transactions = () => {
   const [isActive, setActive] = React.useState('all');
   const [showModal, setShowModal] = React.useState({ show: false });
   const [delay, setDelay] = React.useState(50);
-  const targetEnv = process.env.REACT_APP_MODE.replace('-', '_');
-  const [etherscan] = React.useState(
-    ChainIdMapping[targetEnv].chainName === 'Ethereum Mainnet'
-      ? 'etherscan.io'
-      : 'goerli.etherscan.io',
-  );
 
   const initialPrices = {};
   supportedTokens.forEach(t => {
