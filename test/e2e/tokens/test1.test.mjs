4d41c202efa7d05370064e737852a566d93aa0e6
Liju Jose <liju.jose@gds.ey.com>
2023-02-01 14:26:08 +0530

    test: 36

--- test/e2e/tokens/test1.test.mjs
+++ test/e2e/tokens/test1.test.mjs
@@ -170,7 +170,7 @@ describe('ERC20 tests', () => {
           await getTransactions(environment.clientApiUrl),
         )}---${nf3User2.zkpKeys.compressedZkpPublicKey}`,
       );
-      // await web3Client.waitForEvent(eventLogs, ['transactionSubmitted']);
+      await web3Client.waitForEvent(eventLogs, ['TransactionSubmitted']);
       await makeBlock();
 
       const userL2BalanceAfter = await getLayer2Balances(nf3User, erc20Address);
