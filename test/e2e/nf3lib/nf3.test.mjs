cad79a651ffc12120a0c32627b3670443a18b4c1
Israel Boudoux <iboudoux@polygon.technology>
2023-02-01 11:22:24 +0000

    fix: improving tests

--- test/e2e/nf3lib/nf3.test.mjs
+++ test/e2e/nf3lib/nf3.test.mjs
@@ -1,6 +1,6 @@
 /* eslint no-empty: "off" */
 
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import chaiAsPromised from 'chai-as-promised';
 import config from 'config';
@@ -15,19 +15,24 @@ const environment = config.ENVIRONMENTS[process.env.ENVIRONMENT] || config.ENVIR
 const { mnemonics, signingKeys } = config.TEST_OPTIONS;
 
 describe('Nf3 Test', () => {
-  describe('Client Authenticated calls tests', () => {
+
+  /* This suite of tests require that `nightfall-client` be set with an authentication key (see `AUTHENTICATION_KEY` env var)
+   * for it to work. The value of the authentication key should be set in the environment variable `CLIENT_AUTHENTICATION_KEY`
+   * so that it can be captured inside the tests.
+   */
+  describe('nightfall-client authenticated calls tests', () => {
     it('Should fail since no authentication key is set', async () => {
       const nf3User = new Nf3(signingKeys.user1, environment);
       try {
         await nf3User.init(mnemonics.user1);
         fail();
-      } catch (err) {}
+      } catch (err) {
+        expect(err.isAxiosError && err.response.status === 401).to.be.equal(true);
+      }
     });
 
-    it('Should pass since the authentication key is set', async () => {
-      process.env.CLIENT_AUTHENTICATION_KEY = process.env.AUTHENTICATION_KEY;
-
-      const nf3User = new Nf3(signingKeys.user1, environment);
+    it('Should pass since the client authentication key is set', async () => {
+      const nf3User = new Nf3(signingKeys.user1, environment, undefined, process.env.CLIENT_AUTHENTICATION_KEY);
 
       try {
         await nf3User.init(mnemonics.user1);
