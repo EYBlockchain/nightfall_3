0d6b0a0811c0941bbaff32d3fa390c6e8438cd17
Liju Jose <liju.jose@gds.ey.com>
2023-02-02 13:32:35 +0530

    test: 39

--- nightfall-client/src/event-handlers/transaction-submitted.mjs
+++ nightfall-client/src/event-handlers/transaction-submitted.mjs
@@ -1,7 +1,20 @@
 import logger from '@polygon-nightfall/common-files/utils/logger.mjs';
+import constants from '@polygon-nightfall/common-files/constants/index.mjs';
 import { getTransactionSubmittedCalldata } from '../services/process-calldata.mjs';
-import { zkpPrivateKeys, nullifierKeys } from '../services/keys.mjs';
-import { decryptCommitment } from '../services/commitment-sync.mjs';
+import { countCommitments, countNullifiers } from '../services/commitment-storage.mjs';
+import { saveTransaction } from '../services/database.mjs';
+
+const { ZERO } = constants;
+
+async function doesAnyOfCommitmentsExistInDB(commitments) {
+  const count = await countCommitments(commitments);
+  return Boolean(count);
+}
+
+async function doesAnyOfNullifiersExistInDB(nullifiers) {
+  const count = await countNullifiers(nullifiers);
+  return Boolean(count);
+}
 
 /**
  * This handler runs whenever a new transaction is submitted to the blockchain
@@ -9,20 +22,37 @@ import { decryptCommitment } from '../services/commitment-sync.mjs';
 async function transactionSubmittedEventHandler(eventParams) {
   logger.info(`rrrrrrrrrrrrrrr--- ${JSON.stringify(eventParams)}`);
   const { offchain = false, ...data } = eventParams;
-  let transaction;
-  transaction = data;
-  logger.info(`rrrrrrr11111rrrrrrrr--- ${JSON.stringify(transaction)}`);
-  transaction = await getTransactionSubmittedCalldata(data);
+  let saveTxInDb = false;
+
+  const transaction = await getTransactionSubmittedCalldata(data);
   transaction.blockNumber = data.blockNumber;
   transaction.transactionHashL1 = data.transactionHash;
 
-  const transactionDecrypted = await decryptCommitment(transaction, zkpPrivateKeys, nullifierKeys);
+  // logic: if any of non zero commitment in transaction alraedy exist in db
+  // i.e transaction belong to user using this nightfall-client.
+  // for example: for deposit we store commitment while transaction submit,
+  // similarly for transfer we store change commitment while transaction submit
+
+  // filter out non zero commitments and nullifiers
+  const nonZeroCommitments = transaction.commitments.filter(c => c !== ZERO);
+  const nonZeroNullifiers = transaction.nullifiers.filter(n => n !== ZERO);
+
+  if (await doesAnyOfCommitmentsExistInDB(nonZeroCommitments)) {
+    saveTxInDb = true;
+  } else if (doesAnyOfNullifiersExistInDB(nonZeroNullifiers)) {
+    saveTxInDb = true;
+  }
+
+  if (saveTxInDb) {
+    logger.info({ msg: 'Saving transaction', transaction: transaction.transactionHash });
+    await saveTransaction({ ...transaction });
+  }
 
   logger.info({
     msg: 'Client Transaction Handler - New transaction received.',
     transaction,
     offchain,
-    transactionDecrypted,
+    saveTxInDb,
   });
 }
 
