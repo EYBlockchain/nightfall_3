9981ebbf930440940d95ec67a2c042987aafc7d5
imagobea <himagobea@gmail.com>
2023-02-02 16:46:17 +0100

    feat: client route updated

--- nightfall-client/src/routes/transaction.mjs
+++ nightfall-client/src/routes/transaction.mjs
@@ -2,18 +2,30 @@
  * Routes for interacting with client transactions data.
  */
 import express from 'express';
+import logger from '@polygon-nightfall/common-files/utils/logger.mjs';
 import { getTransactionByTransactionHash } from '../services/database.mjs';
+import { findTransactionInMempools, setL2TransactionStatus } from '../services/transaction.mjs';
 
 const router = express.Router();
 
-router.get('/status/:l2Hash', async (req, res, next) => {
-  const { l2Hash } = req.params;
+/**
+ * The endpoint returns the blockNumberL2 for the specified transaction
+ * If blockNumberL2 => 0, the transaction was 'mined'
+ * If blockNumberL2 === -1, the transaction is in the 'mempool'
+ * Errors: 404 if not found, 400 if result is not valid
+ */
+router.get('/status/:l2TransactionHash', async (req, res, next) => {
+  const { l2TransactionHash } = req.params;
 
   try {
-    const transaction = await getTransactionByTransactionHash(l2Hash);
-    const blockNumberL2 = transaction === null ? -1 : transaction?.blockNumberL2;
-    // TODO - for null results, query proposers mempool (to discuss)
-    res.json({ blockNumberL2 });
+    let transaction = await getTransactionByTransactionHash(l2TransactionHash);
+    if (transaction === null) {
+      logger.debug('Transaction not in Client, check mempools..');
+      transaction = await findTransactionInMempools(l2TransactionHash);
+    }
+    logger.debug({ msg: 'Transaction found', transaction });
+    const status = setL2TransactionStatus(transaction);
+    res.json(status);
   } catch (err) {
     res.json({ error: err.message });
     next(err);
