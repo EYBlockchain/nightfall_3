a1f6a2b494a50104df180283a57fe853a9f93570
Israel Boudoux <iboudoux@polygon.technology>
2023-02-22 21:04:40 +0000

    fix: refactoring to improve readability

--- nightfall-client/src/services/transfer.mjs
+++ nightfall-client/src/services/transfer.mjs
@@ -13,7 +13,10 @@ import {
   compressProof,
 } from '@polygon-nightfall/common-files/utils/curve-maths/curves.mjs';
 import constants from '@polygon-nightfall/common-files/constants/index.mjs';
-import { waitForContract } from '@polygon-nightfall/common-files/utils/contract.mjs';
+import {
+  waitForContract,
+  getFeeL2TokenAddress,
+} from '@polygon-nightfall/common-files/utils/contract.mjs';
 import {
   getCircuitHash,
   generateProof,
@@ -30,30 +33,154 @@ const { VK_IDS } = config;
 const { SHIELD_CONTRACT_NAME, TRANSFER } = constants;
 const { generalise } = gen;
 
-async function transfer(transferParams) {
-  logger.info('Creating a transfer transaction');
-  // let's extract the input items
+const circuitName = TRANSFER;
+
+/**
+ * Does some sort of treatment and validations in the parameters so that they can be used properly
+ */
+async function getTransferParams(transferParams) {
   const {
     offchain = false,
     providedCommitments,
     providedCommitmentsFee,
     ...items
   } = transferParams;
-  const { tokenId, recipientData, rootKey, fee } = generalise(items);
-  const { compressedZkpPublicKey, nullifierKey } = new ZkpKeys(rootKey);
-  const ercAddress = generalise(items.ercAddress.toLowerCase());
-  const { recipientCompressedZkpPublicKeys, values } = recipientData;
-  const recipientZkpPublicKeys = recipientCompressedZkpPublicKeys.map(key =>
-    ZkpKeys.decompressZkpPublicKey(key),
-  );
+  const generalisedItems = generalise(items);
+  const { compressedZkpPublicKey, nullifierKey } = new ZkpKeys(generalisedItems.rootKey);
+  const { recipientCompressedZkpPublicKeys, values } = generalisedItems.recipientData;
+
   if (recipientCompressedZkpPublicKeys.length > 1)
     throw new Error(`Batching is not supported yet: only one recipient is allowed`); // this will not always be true so we try to make the following code agnostic to the number of commitments
 
-  const shieldContractInstance = await waitForContract(SHIELD_CONTRACT_NAME);
-
-  const feeL2TokenAddress = generalise(
-    (await shieldContractInstance.methods.getFeeL2TokenAddress().call()).toLowerCase(),
+  const recipientZkpPublicKeys = recipientCompressedZkpPublicKeys.map(key =>
+    ZkpKeys.decompressZkpPublicKey(key),
   );
+
+  const totalValueToSend = values.reduce((acc, value) => acc + value.bigInt, 0n);
+
+  return {
+    offchain,
+    providedCommitments,
+    providedCommitmentsFee,
+    tokenId: generalisedItems.tokenId,
+    rootKey: generalisedItems.rootKey,
+    fee: generalisedItems.fee,
+    feeL2TokenAddress: await getFeeL2TokenAddress(),
+    ercAddress: generalise(items.ercAddress.toLowerCase()),
+    compressedZkpPublicKey,
+    nullifierKey,
+    values,
+    totalValueToSend,
+    recipientZkpPublicKeys,
+  };
+}
+
+/**
+ * Returns the publicData along with the generated compressed proof for the transfer
+ */
+async function generateTransferProof(
+  fee,
+  feeL2TokenAddress,
+  tokenId,
+  ercAddress,
+  recipientZkpPublicKeys,
+  rootKey,
+  values,
+  commitmentsInfo,
+) {
+  // KEM-DEM encryption
+  const [ePrivate, ePublic] = await genEphemeralKeys();
+  const [unpackedTokenID, packedErc] = packSecrets(tokenId, ercAddress, 0, 2);
+  const compressedSecrets = encrypt(generalise(ePrivate), generalise(recipientZkpPublicKeys[0]), [
+    packedErc.bigInt,
+    unpackedTokenID.bigInt,
+    values[0].bigInt,
+    commitmentsInfo.salts[0].bigInt,
+  ]);
+
+  // Compress the public key as it will be put on-chain
+  const compressedEPub = edwardsCompress(ePublic);
+
+  // now we have everything we need to create a Witness and compute a proof
+  const publicData = new Transaction({
+    fee,
+    historicRootBlockNumberL2: commitmentsInfo.blockNumberL2s,
+    circuitHash: await getCircuitHash(circuitName),
+    ercAddress: compressedSecrets[0], // this is the encrypted ercAddress
+    tokenId: compressedEPub,
+    recipientAddress: compressedSecrets[1], // this is the encrypted tokenID
+    commitments: commitmentsInfo.newCommitments,
+    nullifiers: commitmentsInfo.nullifiers,
+    compressedSecrets: compressedSecrets.slice(2), // these are the [value, salt]
+    numberNullifiers: VK_IDS[circuitName].numberNullifiers,
+    numberCommitments: VK_IDS[circuitName].numberCommitments,
+    isOnlyL2: true,
+  });
+
+  const privateData = {
+    rootKey,
+    oldCommitmentPreimage: commitmentsInfo.oldCommitments.map(o => {
+      return { value: o.preimage.value, salt: o.preimage.salt };
+    }),
+    paths: commitmentsInfo.localSiblingPaths.map(siblingPath => siblingPath.slice(1)),
+    orders: commitmentsInfo.leafIndices,
+    newCommitmentPreimage: commitmentsInfo.newCommitments.map(o => {
+      return { value: o.preimage.value, salt: o.preimage.salt };
+    }),
+    recipientPublicKeys: commitmentsInfo.newCommitments.map(o => o.preimage.zkpPublicKey),
+    ercAddress,
+    tokenId,
+    ephemeralKey: ePrivate,
+  };
+
+  const witness = computeCircuitInputs(
+    publicData,
+    privateData,
+    commitmentsInfo.roots,
+    feeL2TokenAddress,
+    VK_IDS[circuitName].numberNullifiers,
+    VK_IDS[circuitName].numberCommitments,
+  );
+
+  logger.debug({
+    msg: 'witness input is',
+    witness,
+  });
+
+  // call a worker to generate the proof
+  const res = await generateProof({ folderpath: circuitName, witness });
+
+  logger.trace({
+    msg: 'Received response from generate-proof',
+    response: res.data,
+  });
+
+  const { proof } = res.data;
+
+  return { proof: compressProof(proof), publicData };
+}
+
+/**
+ * Transfers funds between Nightfall Accounts.
+ */
+async function transfer(transferParams) {
+  logger.info('Creating a transfer transaction');
+
+  const {
+    offchain,
+    providedCommitments,
+    providedCommitmentsFee,
+    tokenId,
+    rootKey,
+    fee,
+    feeL2TokenAddress,
+    ercAddress,
+    compressedZkpPublicKey,
+    nullifierKey,
+    values,
+    totalValueToSend,
+    recipientZkpPublicKeys,
+  } = await getTransferParams(transferParams);
 
   logger.debug({
     msg: 'Transfer ERC Token & Fee addresses',
@@ -61,9 +188,6 @@ async function transfer(transferParams) {
     feeL2TokenAddress: feeL2TokenAddress.hex(32),
   });
 
-  const circuitName = TRANSFER;
-
-  const totalValueToSend = values.reduce((acc, value) => acc + value.bigInt, 0n);
   const commitmentsInfo = await getCommitmentInfo({
     totalValueToSend,
     fee,
@@ -78,78 +202,18 @@ async function transfer(transferParams) {
   });
 
   try {
-    // KEM-DEM encryption
-    const [ePrivate, ePublic] = await genEphemeralKeys();
-    const [unpackedTokenID, packedErc] = packSecrets(tokenId, ercAddress, 0, 2);
-    const compressedSecrets = encrypt(generalise(ePrivate), generalise(recipientZkpPublicKeys[0]), [
-      packedErc.bigInt,
-      unpackedTokenID.bigInt,
-      values[0].bigInt,
-      commitmentsInfo.salts[0].bigInt,
-    ]);
-
-    // Compress the public key as it will be put on-chain
-    const compressedEPub = edwardsCompress(ePublic);
-
-    const circuitHash = await getCircuitHash(circuitName);
-
-    // now we have everything we need to create a Witness and compute a proof
-    const publicData = new Transaction({
+    const { proof, publicData } = await generateTransferProof(
       fee,
-      historicRootBlockNumberL2: commitmentsInfo.blockNumberL2s,
-      circuitHash,
-      ercAddress: compressedSecrets[0], // this is the encrypted ercAddress
-      tokenId: compressedEPub,
-      recipientAddress: compressedSecrets[1], // this is the encrypted tokenID
-      commitments: commitmentsInfo.newCommitments,
-      nullifiers: commitmentsInfo.nullifiers,
-      compressedSecrets: compressedSecrets.slice(2), // these are the [value, salt]
-      numberNullifiers: VK_IDS[circuitName].numberNullifiers,
-      numberCommitments: VK_IDS[circuitName].numberCommitments,
-      isOnlyL2: true,
-    });
-
-    const privateData = {
-      rootKey,
-      oldCommitmentPreimage: commitmentsInfo.oldCommitments.map(o => {
-        return { value: o.preimage.value, salt: o.preimage.salt };
-      }),
-      paths: commitmentsInfo.localSiblingPaths.map(siblingPath => siblingPath.slice(1)),
-      orders: commitmentsInfo.leafIndices,
-      newCommitmentPreimage: commitmentsInfo.newCommitments.map(o => {
-        return { value: o.preimage.value, salt: o.preimage.salt };
-      }),
-      recipientPublicKeys: commitmentsInfo.newCommitments.map(o => o.preimage.zkpPublicKey),
-      ercAddress,
-      tokenId,
-      ephemeralKey: ePrivate,
-    };
-
-    const witness = computeCircuitInputs(
-      publicData,
-      privateData,
-      commitmentsInfo.roots,
       feeL2TokenAddress,
-      VK_IDS[circuitName].numberNullifiers,
-      VK_IDS[circuitName].numberCommitments,
+      tokenId,
+      ercAddress,
+      recipientZkpPublicKeys,
+      rootKey,
+      values,
+      commitmentsInfo,
     );
 
-    logger.debug({
-      msg: 'witness input is',
-      witness,
-    });
-
-    // call a worker to generate the proof
-    const res = await generateProof({ folderpath: circuitName, witness });
-
-    logger.trace({
-      msg: 'Received response from generate-proof',
-      response: res.data,
-    });
-
-    const { proof } = res.data;
-    // and work out the ABI encoded data that the caller should sign and send to the shield contract
-    const transaction = { ...publicData, proof: compressProof(proof) };
+    const transaction = { ...publicData, proof };
     transaction.transactionHash = Transaction.calcHash(transaction);
 
     logger.debug({
@@ -158,9 +222,12 @@ async function transfer(transferParams) {
       offchain,
     });
 
+    const shieldContractInstance = await waitForContract(SHIELD_CONTRACT_NAME);
+
     const rawTransaction = await shieldContractInstance.methods
       .submitTransaction(Transaction.buildSolidityStruct(transaction))
       .encodeABI();
+
     await submitTransaction(
       transaction,
       commitmentsInfo,
@@ -171,7 +238,7 @@ async function transfer(transferParams) {
 
     return { rawTransaction, transaction };
   } catch (error) {
-    await Promise.all(commitmentsInfo.oldCommitments.map(o => clearPending(o)));
+    await Promise.all(commitmentsInfo.oldCommitments.map(o => clearPending(o))); // TODO this can be improved to be done all at once (via query mongoDB)!
     logger.error(error);
     throw error;
   }
