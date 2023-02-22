ebd639274de1357b09e44b31106d5a5e048a0dfb
Israel Boudoux <iboudoux@polygon.technology>
2023-02-22 20:48:44 +0000

    chore: caching circuitHash

--- common-files/utils/worker-calls.mjs
+++ common-files/utils/worker-calls.mjs
@@ -7,14 +7,19 @@ import config from 'config';
 const { generalise } = gen;
 const { PROTOCOL, CIRCOM_WORKER_HOST, PROVING_SCHEME, BACKEND } = config;
 
+const cachedCircuitHash = {};
+
 // eslint-disable-next-line import/prefer-default-export
 export async function getCircuitHash(circuitName) {
+  if (cachedCircuitHash[circuitName]) return cachedCircuitHash[circuitName];
+
   const responseCircuitHash = await axios.get(`${PROTOCOL}${CIRCOM_WORKER_HOST}/get-circuit-hash`, {
     params: { circuit: circuitName },
   });
 
-  const circuitHash = generalise(responseCircuitHash.data.slice(0, 12)).hex(5);
-  return circuitHash;
+  cachedCircuitHash[circuitName] = generalise(responseCircuitHash.data.slice(0, 12)).hex(5);
+
+  return cachedCircuitHash[circuitName];
 }
 
 export async function generateProof({ folderpath, witness }) {
