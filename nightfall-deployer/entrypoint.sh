a73c234cd332464daff284c8372eddf23eecc4e5
David <druiz0992@gmail.com>
2023-02-24 17:52:44 +0100

    feat: allow serial circuit setup

--- nightfall-deployer/entrypoint.sh
+++ nightfall-deployer/entrypoint.sh
@@ -7,4 +7,17 @@ if [ -z "${ETH_PRIVATE_KEY}" ]; then
   while ! nc -z ${BLOCKCHAIN_WS_HOST} ${BLOCKCHAIN_PORT}; do sleep 3; done
 fi
 
+if [[ "${SKIP_DEPLOYMENT}" != "true" && "${PARALLEL_SETUP}" == "true" ]]; then
+  npx truffle compile --all
+
+  if [ -z "${UPGRADE}" ]; then
+    echo "Deploying contracts to ${ETH_NETWORK}"
+    npx truffle migrate --to 3 --network=${ETH_NETWORK}
+    echo 'Done'
+  else
+    echo 'Upgrading contracts'
+    npx truffle migrate -f 4 --network=${ETH_NETWORK} --skip-dry-run
+  fi
+fi
+
 npm start
