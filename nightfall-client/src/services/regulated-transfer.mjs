9548ad98ae5bf49bb4a3531d60ca4bdf0c9f8273
Rajeev <rajeev.sahu1@uk.ey.com>
2023-02-24 00:12:54 +0000

    feat: added files for regulated transfer

--- nightfall-client/src/services/regulated-transfer.mjs
+++ nightfall-client/src/services/regulated-transfer.mjs
@@ -98,7 +98,7 @@ async function transfer(transferParams) {
       generalise(ePrivate),
       generalise(recipientRegulatorZkpPublicKeys[0]),
       [packedErc.bigInt, unpackedTokenID.bigInt, values[0].bigInt, commitmentsInfo.salts[0].bigInt,]
-  );
+      );
 
     // Compress the public keys as they will be put on-chain
 
