8edf32f118c53c0ea7e94c621f01e088e526eca4
Westlad <Duncan.Westland@uk.ey.com>
2023-02-24 16:42:21 +0000

    feat: nist test vectors passing

--- test/unit/SmartContracts/sha.unit.test.mjs
+++ test/unit/SmartContracts/sha.unit.test.mjs
@@ -1,13 +1,44 @@
 import { expect } from 'chai';
 import hardhat from 'hardhat';
+import fs from 'fs';
 
 const { ethers } = hardhat;
 
+function readNistVectors(filename) {
+  const payload = line => line.split('=')[1].trim();
+  const file = fs.readFileSync(filename);
+  const lines = file.toString().split('\n');
+  const nistVectors = [];
+  let len;
+  let msg;
+  let md;
+  for (const line of lines) {
+    if (line.includes('#')) continue; // eslint-disable-line no-continue
+    if (line.includes('Len')) len = parseInt(payload(line), 10);
+    else if (line.includes('Msg')) msg = `0x${payload(line)}`;
+    else if (line.includes('MD')) {
+      // if we get here, we've seen all three lines that make up a test vector, so process the last line and store the vector
+      md = `0x${payload(line)}`;
+      if (len !== 0) nistVectors.push({ len, msg, md });
+    }
+    if (len >= 32768) break; // anything longer than this takes ages to comput and it's bigger than a cert is likely to be
+  }
+  return nistVectors;
+}
+
 describe('SHA512 tests', function () {
   let ShaInstance;
+  let nistVectors;
+  const message448 =
+    '0x6162636462636465636465666465666765666768666768696768696a68696a6b696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f7071';
   before(async () => {
     const ShaDeployer = await ethers.getContractFactory('Sha');
     ShaInstance = await ShaDeployer.deploy();
+    const nistVectorsShort = readNistVectors(
+      'test/unit/utils/nist_test_vectors/SHA512ShortMsg.rsp',
+    );
+    const nistVectorsLong = readNistVectors('test/unit/utils/nist_test_vectors/SHA512LongMsg.rsp');
+    nistVectors = [...nistVectorsShort, ...nistVectorsLong];
   });
   it('Should correctly shift right', async function () {
     const t1 = {
@@ -43,15 +74,44 @@ describe('SHA512 tests', function () {
     const rs3 = (await ShaInstance.ROTR(2, t3.in)).mask(64);
     expect(rs3).to.be.equal(t3.out);
   });
-  it('Should correctly compute all test vectors', async function () {
-    const t1 = {
-      in: '0x616263',
-      out: '0xddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f',
-      out256: '0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad',
+  it('Should correctly pad the message', async function () {
+    const paddedMessage = await ShaInstance.padMessage1024(message448);
+    expect(paddedMessage.length).to.be.equal(128 * 2 + 2); // 128 bytes (1024 bits)
+    expect(message448.concat('80')).to.be.equal(paddedMessage.slice(0, message448.length + 2));
+    expect(BigInt(`0x${paddedMessage.slice(-32)}`)).to.be.equal(448n);
+  });
+  it('Should correctly parse a padded message into message blocks', async function () {
+    const paddedMessage = await ShaInstance.padMessage1024(message448); // bytes resolves to a hex string
+    const parsedMessage = await ShaInstance.parseMessage1024(paddedMessage); // bytes resolves to a hex string
+    expect(parsedMessage[0]).to.be.equal(paddedMessage); // the padded message is only one block long
+    expect(parsedMessage.length).to.be.equal(1);
+  });
+  it('Should correctly separate a message block into words', async function () {
+    const paddedMessage = await ShaInstance.padMessage1024(message448);
+    const parsedMessage = await ShaInstance.parseMessage1024(paddedMessage);
+    const words = await ShaInstance.parseMessageBlock1024(parsedMessage[0]);
+    const tests = words.map(w => w.toHexString().slice(2).padStart(16, '0')); // remove the 0x
+    let test = '';
+    for (let i = 0; i < tests.length; i++) {
+      test = test.concat(tests[i]);
+    }
+    test = `0x${test}`;
+    expect(words.length).to.be.equal(16);
+    expect(paddedMessage).to.be.equal(test); // if we reassemble the words, do we get the same answer?
+  });
+  it('Should correctly compute the NIST test vectors < 32kb in size', async function () {
+    // collect results asynchronously as this is a long test
+    const checkResult = async test => {
+      expect(await test.result).to.be.equal(
+        test.vector.md,
+        `Test failed for input ${test.vector.msg}, of length ${test.vector.len}`,
+      );
     };
-    const check = await ShaInstance._sha256(t1.in);
-    expect(check).to.equal(t1.out256);
-    const r1 = await ShaInstance.sha512(t1.in);
-    expect(r1).to.equal(t1.out);
+    const nistResults = [];
+    for (const vector of nistVectors) {
+      const result = ShaInstance.sha512(vector.msg);
+      nistResults.push(checkResult({ result, vector }));
+    }
+    await Promise.all(nistResults);
   });
 });
