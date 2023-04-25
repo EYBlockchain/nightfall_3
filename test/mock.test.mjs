87eb170bc7428745626582e07fb17085a71f9dcc
David <druiz0992@gmail.com>
2023-04-25 23:05:39 +0200

    fix: remove mock

--- test/mock.test.mjs
+++
@@ -1,7 +0,0 @@
-import { syncState } from '../nightfall-client/src/services/state-sync.mjs';
-
-describe('Mock Tests', () => {
-  it('Check all mocks ', async () => {
-    await syncState();
-  });
-});
