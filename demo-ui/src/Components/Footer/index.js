448ff71e9aa9e51b97ad591f11bfb9a9508df9ae
Liju Jose <liju.jose@gds.ey.com>
2023-05-04 21:05:40 +1000

    fix: ey logo removed

--- demo-ui/src/Components/Footer/index.js
+++ demo-ui/src/Components/Footer/index.js
@@ -5,12 +5,8 @@ import './index.css';
 function Footer() {
   return (
     <footer className="footer text-center text-lg-start bg-white text-muted fixed-bottom row">
-      <div className="text-center col-10"></div>
-      <div className="text-center col-2">
-        <h1>
-          <strong>EY</strong>
-        </h1>
-      </div>
+      {/* <div className="text-center col-10"></div>
+      <div className="text-center col-2"></div> */}
     </footer>
   );
 }
