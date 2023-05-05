b3b3c734a1cb57c2c2e7aaac6cf01476abe4d279
Liju Jose <liju.jose@gds.ey.com>
2023-05-05 14:18:48 +1000

    fix: now mnemonic from config can be selected

--- demo-ui/src/Components/AddUser/index.js
+++ demo-ui/src/Components/AddUser/index.js
@@ -1,17 +1,24 @@
 // ignore unused exports
 import React from 'react';
-
+import config from 'config'; // eslint-disable-line  import/no-extraneous-dependencies
 import './index.css';
 
 function AddUser({ addNewUser }) {
   const [userName, setUserName] = React.useState('');
   const [userMnemonic, setUserMnemonic] = React.useState('');
+  const [mnemonics, setUserMnemonics] = React.useState([
+    ...new Set(Object.entries(config.TEST_OPTIONS.mnemonics).map(a => a[1])),
+  ]);
 
   function submitNewUser(e) {
     e.preventDefault();
     addNewUser(userName, userMnemonic);
     setUserName('');
     setUserMnemonic('');
+
+    // remove used mnemonic from array
+    mnemonics.splice(mnemonics.indexOf(userMnemonic), 1);
+    setUserMnemonics(mnemonics);
   }
 
   return (
@@ -27,15 +34,38 @@ function AddUser({ addNewUser }) {
               onChange={e => setUserName(e.target.value)}
             />
           </div>
-          <div className="form-group form-custom-field">
+          <div className="form-group form-custom-field-d">
             <input
               type="text"
               className="form-control"
               placeholder="User Mnemonic"
               value={userMnemonic}
+              style={{ width: '74%', display: 'inline-block', marginRight: '10px' }}
               onChange={e => setUserMnemonic(e.target.value)}
             />
+            or
+            <select
+              className="form-select"
+              aria-label="Default select example"
+              value={userMnemonic}
+              style={{ width: '10%', display: 'inline-block', marginLeft: '10px' }}
+              onChange={e => setUserMnemonic(e.target.value)}
+            >
+              <option value=""> </option>
+              {mnemonics.map(value => (
+                <option key={Math.random()} value={value}>
+                  {value}
+                </option>
+              ))}
+            </select>
+            <label>
+              <small>
+                Enter or copy paste mnemonic in input box, or select from options from config (only
+                applicable in test local ganache)
+              </small>
+            </label>
           </div>
+          <div className="form-group form-custom-field"></div>
           <div className="form-group form-custom-field">
             <button type="button" className="btn btn-primary" onClick={submitNewUser}>
               Add User
